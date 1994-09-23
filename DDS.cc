cb507e53b0a1a5225db7ac6666a82076cbe16632
James Gallagher <jgallagher@opendap.org>
1994-09-23 14:42:23 +0000

    Added mfunc check_semantics(). Replaced print mfunc stub with real code. Fixed some errors in comments.

--- DDS.cc
+++ DDS.cc
@@ -1,13 +1,17 @@
 
-// Methods for the class DDS - a class used to parse the dataset descriptor
-// structure.
+// Methods for class DDS
 //
 // jhrg 9/7/94
 
 // $Log: DDS.cc,v $
-// Revision 1.2  1994/09/15 21:08:39  jimg
+// Revision 1.3  1994/09/23 14:42:22  jimg
+// Added mfunc check_semantics().
+// Replaced print mfunc stub with real code.
+// Fixed some errors in comments.
+//
+// Revision 1.2  1994/09/15  21:08:39  jimg
 // Added many classes to the BaseType hierarchy - the complete set of types
-// described in the DODS API design documet is not represented.
+// described in the DODS API design documet is now represented.
 // The parser can parse DDS files.
 // Fixed many small problems with BaseType.
 // Added CtorType.
@@ -16,7 +20,7 @@
 // First version of the Dataset descriptor class.
 // 
 
-static char rcsid[]="$Id: DDS.cc,v 1.2 1994/09/15 21:08:39 jimg Exp $";
+static char rcsid[]="$Id: DDS.cc,v 1.3 1994/09/23 14:42:22 jimg Exp $";
 
 #ifdef __GNUG__
 #pragma implementation
@@ -25,16 +29,21 @@ static char rcsid[]="$Id: DDS.cc,v 1.2 1994/09/15 21:08:39 jimg Exp $";
 #include <stdio.h>
 
 #include <iostream.h>
+#ifdef NEVER
 #include <stdiostream.h>
+#endif
 #include <String.h>
 
+#ifdef NEVER
 #include <sys/types.h>
 #include <sys/stat.h>
 
 #include <unistd.h>
 #include <fcntl.h>
+#endif
 
 #include "DDS.h"
+#include "util.h"
 
 int ddsrestart(FILE *yyin);
 int ddsparse(DDS &table);	// defined in dds.tab.c
@@ -43,12 +52,8 @@ DDS::DDS(const String &n) : name(n)
 {
 }
 
-DDS::~DDS()
-{
-}
-
 String 
-DDS::get_dataset_name() 
+DDS::get_dataset_name()
 { 
     return name; 
 }
@@ -62,7 +67,6 @@ DDS::set_dataset_name(const String &n)
 void
 DDS::add_var(BaseType *bt)
 { 
-    cout << "Adding: " << bt->get_var_name() << " @ " << bt << endl; 
     vars.append(bt); 
 }
 
@@ -86,10 +90,12 @@ DDS::var(const String &n)
     for (Pix p = vars.first(); p; vars.next(p))
 	if (vars(p)->get_var_name() == n)
 	    return vars(p);
+
+    return 0;
 }
 
 Pix 
-DDS::first_var() 
+DDS::first_var()
 { 
     return vars.first(); 
 }
@@ -97,13 +103,15 @@ DDS::first_var()
 void 
 DDS::next_var(Pix &p) 
 { 
-    vars.next(p); 
+    if (!vars.empty())
+	vars.next(p); 
 }
 
 BaseType *
-DDS::var(Pix p) 
+DDS::var(Pix p)
 { 
-    return vars(p); 
+    if (!vars.empty() && p)
+	return vars(p); 
 }
 
 // Read structure from IN (which defaults to stdin). If ddsrestart() fails,
@@ -112,6 +120,9 @@ DDS::var(Pix p)
 bool
 DDS::parse(FILE *in)
 {
+    if (!in)
+	return false;
+
     if (!ddsrestart(in)) {
 	cerr << "Could not read from input source" << endl;
 	return false;
@@ -126,5 +137,41 @@ DDS::parse(FILE *in)
 bool
 DDS::print(FILE *out)
 {
+    cout << "Dataset {" << endl;
+
+    for (Pix p = vars.first(); p; vars.next(p))
+	vars(p)->print_decl();
+
+    cout << "} " << name << ";" << endl;
+					   
+    return true;
+}
+
+// Check the semantics of the DDS describing a complete dataset. If ALL is
+// true, check not only the semantics of THIS->TABLE, but also recurrsively
+// all ctor types in the THIS->TABLE. By default, ALL is false since parsing
+// a DDS input file runns semantic checks on all variables (but not the
+// dataset itself.
+//
+// Returns: true if the conventions for the DDS are not violated, false
+// otherwise. 
+
+bool
+DDS::check_semantics(bool all)
+{
+    // The dataset must have a name
+    if (name == (char *)0) {
+	cerr << "A dataset must have a name" << endl;
+	return false;
+    }
+
+    if (!unique(vars, (const char *)name, (const char *)"Dataset"))
+	return false;
+
+    if (all) 
+	for (Pix p = vars.first(); p; vars.next(p))
+	    if (!vars(p)->check_semantics(true))
+		return false;
+
     return true;
 }
--- DDS.h
+++ DDS.h
@@ -1,17 +1,24 @@
-// This may look like C code, but it is really -*- C++ -*-
+// -*- C++ -*-
 
-// Using the DDSVHMap class, build a parser for the DDS.
+// Provide access to the DDS. This class is used to parse DDS text files, to
+// produce a printed representation of the in-memory variable table, and to
+// update the table on a per-variable basis.
 //
 // jhrg 9/8/94
 
 /* $Log: DDS.h,v $
-/* Revision 1.2  1994/09/15 21:09:00  jimg
-/* Added many classes to the BaseType hierarchy - the complete set of types
-/* described in the DODS API design documet is not represented.
-/* The parser can parse DDS files.
-/* Fixed many small problems with BaseType.
-/* Added CtorType.
+/* Revision 1.3  1994/09/23 14:42:23  jimg
+/* Added mfunc check_semantics().
+/* Replaced print mfunc stub with real code.
+/* Fixed some errors in comments.
 /*
+ * Revision 1.2  1994/09/15  21:09:00  jimg
+ * Added many classes to the BaseType hierarchy - the complete set of types
+ * described in the DODS API design documet is now represented.
+ * The parser can parse DDS files.
+ * Fixed many small problems with BaseType.
+ * Added CtorType.
+ *
  * Revision 1.1  1994/09/08  21:09:42  jimg
  * First version of the Dataset descriptor class.
  */
@@ -39,7 +46,7 @@ private:
 public:
     DDS(const String &n = (char *)0);
     // Use the default copy ctor and op=
-    virtual ~DDS();
+    virtual ~DDS() {}
 
     String get_dataset_name();
     void set_dataset_name(const String &n);
@@ -54,8 +61,8 @@ public:
 
     // Interface to the parser
     bool parse(FILE *in=stdin);
-
     bool print(FILE *out=stdout);
+    bool check_semantics(bool all = false);
 };
 
 #endif
