ee812041ce2810b564ecbff0b3e549f70dac7631
James Gallagher <jgallagher@opendap.org>
1994-09-27 22:42:45 +0000

    Changed definition of the class AttrTable; it no longer inherits from AttrVHMap, instead it uses that class (contains a member that is an instance of AttrVHMap). Added mfuncs to AttrTable so that the new member could be set/accessed.

--- AttrTable.cc
+++ AttrTable.cc
@@ -4,7 +4,13 @@
 // jhrg 7/29/94
 
 // $Log: AttrTable.cc,v $
-// Revision 1.4  1994/09/09 15:26:39  jimg
+// Revision 1.5  1994/09/27 22:42:44  jimg
+// Changed definition of the class AttrTable; it no longer inherits from
+// AttrVHMap, instead it uses that class (contains a member that is an instance
+// of AttrVHMap).
+// Added mfuncs to AttrTable so that the new member could be set/accessed.
+//
+// Revision 1.4  1994/09/09  15:26:39  jimg
 // Removed operator<< and added print() since I have no good way to define
 // operator>>. It seems best to define all operators from a set (like <<, >>)
 // or none at all. Since parse() is the input mfunc, it seems that output
@@ -16,12 +22,18 @@
 //
 // Revision 1.2  1994/08/02  19:17:37  jimg
 // Fixed `$Log: AttrTable.cc,v $
-// Fixed `Revision 1.4  1994/09/09 15:26:39  jimg
-// Fixed `Removed operator<< and added print() since I have no good way to define
-// Fixed `operator>>. It seems best to define all operators from a set (like <<, >>)
-// Fixed `or none at all. Since parse() is the input mfunc, it seems that output
-// Fixed `should be a mfunc too.
+// Fixed `Revision 1.5  1994/09/27 22:42:44  jimg
+// Fixed `Changed definition of the class AttrTable; it no longer inherits from
+// Fixed `AttrVHMap, instead it uses that class (contains a member that is an instance
+// Fixed `of AttrVHMap).
+// Fixed `Added mfuncs to AttrTable so that the new member could be set/accessed.
 // Fixed `
+// Revision 1.4  1994/09/09  15:26:39  jimg
+// Removed operator<< and added print() since I have no good way to define
+// operator>>. It seems best to define all operators from a set (like <<, >>)
+// or none at all. Since parse() is the input mfunc, it seems that output
+// should be a mfunc too.
+//
 // Revision 1.3  1994/08/02  20:11:27  jimg
 // Changes operator<< so that it writes a parsable version of the
 // attribute table.
@@ -34,21 +46,67 @@
 // a static class variable String empty (it is initialized to "").
 //
 
-static char rcsid[]="$Id: AttrTable.cc,v 1.4 1994/09/09 15:26:39 jimg Exp $";
+static char rcsid[]="$Id: AttrTable.cc,v 1.5 1994/09/27 22:42:44 jimg Exp $";
 
 #ifdef __GNUG__
 #pragma implementation
 #endif
-#include "AttrTable.h"
 
 #include <ostream.h>
 #include <Pix.h>
 
-String AttrTable::empty = "";	// this must only be defined once.
+#include "AttrTable.h"
+
+AttrTable::AttrTable(String& dflt, unsigned int sz) : map(dflt, sz)
+{
+}
+
+Pix 
+AttrTable::first_attr()
+{
+    return map.first();
+}
 
 void
-AttrTable::print(ostream &os)
+AttrTable::next_attr(Pix &p)
 {
-    for(Pix p = first(); p; next(p))
-	os << key(p) << " " << contents(p) << ";" << endl;
+    map.next(p);
 }
+
+String &
+AttrTable::get_name(Pix p)
+{
+    return map.key(p);
+}
+
+String &
+AttrTable::get_attr(Pix p)
+{
+    return map.contents(p);
+}
+
+String &
+AttrTable::get_attr(const String &name)
+{
+    return map[name];
+}
+
+String &
+AttrTable::get_attr(const char *name)
+{
+    return map[name];
+}
+
+void
+AttrTable::set_attr(const String &name, String value)
+{
+    map[name] = value;
+}
+
+void
+AttrTable::print(ostream &os, String pad)
+{
+    for(Pix p = map.first(); p; map.next(p))
+	os << pad << map.key(p) << " " << map.contents(p) << ";" << endl;
+}
+
--- AttrTable.h
+++ AttrTable.h
@@ -10,13 +10,19 @@
 // a temporary object according to g++'s warnings.
 
 /* $Log: AttrTable.h,v $
-/* Revision 1.4  1994/09/15 21:08:54  jimg
-/* Added many classes to the BaseType hierarchy - the complete set of types
-/* described in the DODS API design documet is not represented.
-/* The parser can parse DDS files.
-/* Fixed many small problems with BaseType.
-/* Added CtorType.
+/* Revision 1.5  1994/09/27 22:42:45  jimg
+/* Changed definition of the class AttrTable; it no longer inherits from
+/* AttrVHMap, instead it uses that class (contains a member that is an instance
+/* of AttrVHMap).
+/* Added mfuncs to AttrTable so that the new member could be set/accessed.
 /*
+ * Revision 1.4  1994/09/15  21:08:54  jimg
+ * Added many classes to the BaseType hierarchy - the complete set of types
+ * described in the DODS API design documet is not represented.
+ * The parser can parse DDS files.
+ * Fixed many small problems with BaseType.
+ * Added CtorType.
+ *
  * Revision 1.3  1994/09/09  15:26:41  jimg
  * Removed operator<< and added print() since I have no good way to define
  * operator>>. It seems best to define all operators from a set (like <<, >>)
@@ -25,13 +31,19 @@
  *
  * Revision 1.2  1994/08/02  19:17:39  jimg
  * Fixed `$Log: AttrTable.h,v $
- * Fixed `Revision 1.4  1994/09/15 21:08:54  jimg
- * Fixed `Added many classes to the BaseType hierarchy - the complete set of types
- * Fixed `described in the DODS API design documet is not represented.
- * Fixed `The parser can parse DDS files.
- * Fixed `Fixed many small problems with BaseType.
- * Fixed `Added CtorType.
+ * Fixed `Revision 1.5  1994/09/27 22:42:45  jimg
+ * Fixed `Changed definition of the class AttrTable; it no longer inherits from
+ * Fixed `AttrVHMap, instead it uses that class (contains a member that is an instance
+ * Fixed `of AttrVHMap).
+ * Fixed `Added mfuncs to AttrTable so that the new member could be set/accessed.
  * Fixed `
+ * Revision 1.4  1994/09/15  21:08:54  jimg
+ * Added many classes to the BaseType hierarchy - the complete set of types
+ * described in the DODS API design documet is not represented.
+ * The parser can parse DDS files.
+ * Fixed many small problems with BaseType.
+ * Added CtorType.
+ *
  * Revision 1.3  1994/09/09  15:26:41  jimg
  * Removed operator<< and added print() since I have no good way to define
  * operator>>. It seems best to define all operators from a set (like <<, >>)
@@ -51,39 +63,39 @@
  */
 
 #ifndef _AttrTable_h
+#define _AttrTable_h 1
+
 #ifdef __GNUG__
 #pragma interface
 #endif
-#define _AttrTable_h 1
+
+#include <Pix.h>
+#include <String.h>
 
 #include "AttrVHMap.h"
 
-class AttrTable : public AttrVHMap {
+class AttrTable {
 private:
-    static String empty;	// defined in AttrTable.cc
+    AttrVHMap map;		// mapping of names to values
 
 protected:
     
 public:
-    AttrTable(String& dflt=empty, unsigned int sz=DEFAULT_INITIAL_CAPACITY);
-    AttrTable(AttrTable& a);
-    ~AttrTable();
+    AttrTable(String& dflt=(char *)0, 
+	      unsigned int sz=DEFAULT_INITIAL_CAPACITY);
 
-    void print(ostream &os);
+    Pix first_attr();
+    void next_attr(Pix &p);
+    String &get_name(Pix p);
+    String &get_attr(Pix p);
+
+    String &get_attr(const String &name);
+    String &get_attr(const char *);
+    void set_attr(const String &name, String value);
+
+    void print(ostream &os, String pad = "    ");
 };
 
 typedef AttrTable * AttrTablePtr;
-
-AttrTable::AttrTable(String& dflt, unsigned int sz) : AttrVHMap(dflt, sz)
-{
-}
-
-inline AttrTable::AttrTable(AttrTable& a) : AttrVHMap(a)
-{
-}
-
-inline AttrTable::~AttrTable()
-{
-}
 
 #endif
