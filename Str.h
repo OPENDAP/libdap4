595eb5789962da1fc00c4708899ccdf5ef0679e5
Reza Nekovei <rnekovei@ieee.org>
1994-11-03 04:43:07 +0000

    Changed the default type to match the dds parser.

--- Str.h
+++ Str.h
@@ -7,9 +7,12 @@
 // jhrg 9/7/94
 
 /* $Log: Str.h,v $
-/* Revision 1.2  1994/09/23 14:36:14  jimg
-/* Fixed errors in comments.
+/* Revision 1.3  1994/11/03 04:43:07  reza
+/* Changed the default type to match the dds parser.
 /*
+ * Revision 1.2  1994/09/23  14:36:14  jimg
+ * Fixed errors in comments.
+ *
  * Revision 1.1  1994/09/15  21:09:08  jimg
  * Added many classes to the BaseType hierarchy - the complete set of types
  * described in the DODS API design documet is now represented.
@@ -35,7 +38,7 @@ class Str: public BaseType {
 private:
 
 public:
-    Str(const String &n = (char *)0, const String &t = "Str");
+    Str(const String &n = (char *)0, const String &t = "String");
     virtual ~Str() {}
 };
 
