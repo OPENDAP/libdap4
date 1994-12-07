a4a2354f4b0d6e390ad50cd476a909b5726ee5ef
James Gallagher <jgallagher@opendap.org>
1994-12-07 21:21:47 +0000

    Added code to test the scanner - it is selected using the -s option.

--- das-test.cc
+++ das-test.cc
@@ -7,7 +7,10 @@
 // jhrg 7/25/94
 
 // $Log: das-test.cc,v $
-// Revision 1.5  1994/10/13 16:12:07  jimg
+// Revision 1.6  1994/12/07 21:21:47  jimg
+// Added code to test the scanner - it is selected using the -s option.
+//
+// Revision 1.5  1994/10/13  16:12:07  jimg
 // Added -p to interface: run the parser. This option makes das-test
 // behave as it used to, with the following exceptions:
 // 1) if `none' is given as a filename, then the associated actions (reading
@@ -36,20 +39,31 @@
 #include <Pix.h>
 #include <GetOpt.h>
 
-#include "DAS.h"
+#define YYSTYPE char *
 
-void parser_driver(char *argv[], int i, bool use_fd, DAS das);
+#include "DAS.h"
+#include "das.tab.h"
+
+void parser_driver(int argc, char *argv[], int i, bool use_fd, DAS das);
 void plain_driver(DAS das);
 void load_attr_table(AttrTable at);
 void load_attr_table_ptr(AttrTable *atp);
+void test_scanner();
+
+int daslex();
+int dasparse(DAS &);
+
+extern int dasdebug;
+const char *prompt = "das-test: ";
 
 int
 main(int argc, char *argv[])
 {
-    GetOpt getopt (argc, argv, "fp");
+    GetOpt getopt (argc, argv, "sfp");
     int option_char;
     bool use_fd = false;	// true to exercise the fd functions
     bool test_parser = false;
+    bool scanner_test = false;
 
     // process options first so that debugging in on for object instantitaion.
     while ((option_char = getopt ()) != EOF)
@@ -61,6 +75,9 @@ main(int argc, char *argv[])
 	    case 'f':
 	      use_fd = true;
 	      break;
+	    case 's':
+	      scanner_test = true;
+	      break;
 	    case '?': 
 	      cerr << "usage: " << argv[0] << " [f] [p in-file1 out-file1 ...]"
 		   << endl;
@@ -70,13 +87,55 @@ main(int argc, char *argv[])
 
     if (test_parser)
 	for (int i = getopt.optind; i < argc; i+=2)
-	    parser_driver(argv, i, use_fd, das);
+	    parser_driver(argc, argv, i, use_fd, das);
+    else if (scanner_test)
+	test_scanner();
     else
 	plain_driver(das);
 }
 
 void
-parser_driver(char *argv[], int i, bool use_fd, DAS das)
+test_scanner(void)
+{
+    int tok;
+
+    cout << prompt;		// first prompt
+    while ((tok = daslex())) {
+	switch (tok) {
+	  case ATTR:
+	    cout << "ATTR" << endl;
+	    break;
+	  case ID:
+	    cout << "ID=" << daslval << endl;
+	    break;
+	  case VAL:
+	    cout << "VAL=" << daslval << endl;
+	    break;
+	  case TYPE:
+	    cout << "TYPE" << endl;
+	    break;
+	  case '{':
+	    cout << "Left Brace" << endl;
+	    break;
+	  case '}':
+	    cout << "Right Brace" << endl;
+	    break;
+	  case ';':
+	    cout << "Semicolon" << endl;
+	    break;
+	  case ',':
+	    cout << "Comma" << endl;
+	    break;
+	  default:
+	    cout << "Error: Unrecognized input" << endl;
+	}
+	cout << prompt;		// print prompt after output
+    }
+}
+
+
+void
+parser_driver(int argc, char *argv[], int i, bool use_fd, DAS das)
 {
     // If a file is named "-", assume that the user means stdin or stdout.
     if (strcmp(argv[i], "none") == 0)
@@ -104,23 +163,10 @@ parser_driver(char *argv[], int i, bool use_fd, DAS das)
     }
 
     // filename of "none" means don't print.
-    if (strcmp(argv[i+1], "none") == 0)
-	return;
-    else if (strcmp(argv[i+1], "-") == 0)
+    if (argc > i+1 && strcmp(argv[i+1], "-") == 0)
 	das.print();
-    else {
+    else
 	return;
-#ifdef NEVER
-	cout << "Writing to: " << argv[i+1] << endl;
-	if (use_fd) {
-	    int fd = open(argv[i], O_WRONLY);
-	    das.print(fd);
-	    close(fd);
-	}
-	else
-	    das.print(argv[i+1]);
-#endif
-    }
 }
 
 // Given a DAS, add some stuff to it.
@@ -131,18 +177,21 @@ plain_driver(DAS das)
     AttrTable *atp;
     AttrTable *dummy;
 
-    for (int i = 0; i < 4; ++i) {
-	String name = "test" + i;
-	atp = new AttrTable;
-	load_attr_table_ptr(atp);
+    String name = "test";
+    atp = new AttrTable;
+    load_attr_table_ptr(atp);
+    dummy = das.get_table(name);
+    das.add_table(name, atp);
 
-	dummy = das.get_table(name);
-	das.add_table(name, atp);
-    }
+    name = "test2";
+    atp = new AttrTable;
+    load_attr_table_ptr(atp);
+    das.add_table(name, atp);
 
     das.print();
 }
 
+#ifdef NEVER
 // stuff an AttrTable full of values. Also, print it out.
 
 void
@@ -150,44 +199,103 @@ load_attr_table(AttrTable at)
 {
     at.append_attr("month", "String", "Feb");
     at.append_attr("month", "String", "Feb");
+
+    at.append_attr("month_a", "String", "Jan");
+    at.append_attr("month_a", "String", "Feb");
+    at.append_attr("month_a", "String", "Mar");
+
     at.append_attr("Date", "Int32", "12345");
     at.append_attr("day", "Int32", "01");
     at.append_attr("Time", "Float64", "3.1415");
 
     cout << "Using the Pix:" << endl;
     for (Pix p = at.first_attr(); p; at.next_attr(p)) {
-	cout << at.get_name(p) << " " << at.get_type(p) << " " 
-	     << at.get_attr(p) << endl;
+	cout << at.get_name(p) << " " << at.get_type(p) << " ";
+	for (int i = 0; i < at.get_attr_num(p); ++i)
+	     cout << at.get_attr(p, i) << " ";
+	cout << endl;
     }
 
     String name = "month";
-    cout << "Using String: " << at.get_type(name) << " " << at.get_attr(name) 
-	 << endl;
+    cout << "Using String: " << at.get_type(name) << " " 
+	 << at.get_attr(name, 0) << " " << at.get_attr(name, 1) << endl;
     cout << "Using char *: " << at.get_type("month") << " " 
-	 << at.get_attr("month") << endl;
+	 << at.get_attr("month", 0) << " " << at.get_attr("month", 1) << endl;
 
     at.del_attr("month");
 
     cout << "After deletion:" << endl;
     for (p = at.first_attr(); p; at.next_attr(p)) {
-	cout << at.get_name(p) << " " << at.get_type(p) << " " 
-	     << at.get_attr(p) << endl;
+	cout << at.get_name(p) << " " << at.get_type(p) << " ";
+	for (int i = 0; i < at.get_attr_num(p); ++i)
+	     cout << at.get_attr(p, i) << " ";
+	cout << endl;
     }
 
     at.print(cout);
 
     cout << "After print:" << endl;
     for (p = at.first_attr(); p; at.next_attr(p)) {
-	cout << at.get_name(p) << " " << at.get_type(p) << " " 
-	     << at.get_attr(p) << endl;
+	cout << at.get_name(p) << " " << at.get_type(p) << " ";
+	for (int i = 0; i < at.get_attr_num(p); ++i)
+	     cout << at.get_attr(p, i) << " ";
+	cout << endl;
     }
 }
+#endif
 
 // OK, now try it with a dymanic AttrTable
 
 void
-load_attr_table_ptr(AttrTable *atp)
+load_attr_table_ptr(AttrTable *at)
 {
+    at->append_attr("month", "String", "Feb");
+    at->append_attr("month", "String", "Feb");
+
+    at->append_attr("month_a", "String", "Jan");
+    at->append_attr("month_a", "String", "Feb");
+    at->append_attr("month_a", "String", "Mar");
+
+    at->append_attr("Date", "Int32", "12345");
+    at->append_attr("day", "Int32", "01");
+    at->append_attr("Time", "Float64", "3.1415");
+
+    cout << "Using the Pix:" << endl;
+    for (Pix p = at->first_attr(); p; at->next_attr(p)) {
+	cout << at->get_name(p) << " " << at->get_type(p) << " ";
+	for (int i = 0; i < at->get_attr_num(p); ++i)
+	     cout << at->get_attr(p, i) << " ";
+	cout << endl;
+    }
+
+    String name = "month";
+    cout << "Using String: " << at->get_type(name) << " " 
+	 << at->get_attr(name, 0) << " " << at->get_attr(name, 1) << endl;
+    cout << "Using char *: " << at->get_type("month") << " " 
+	 << at->get_attr("month", 0) << " " << at->get_attr("month", 1) << endl;
+
+    at->del_attr("month");
+
+    cout << "After deletion:" << endl;
+    for (p = at->first_attr(); p; at->next_attr(p)) {
+	cout << at->get_name(p) << " " << at->get_type(p) << " ";
+	for (int i = 0; i < at->get_attr_num(p); ++i)
+	     cout << at->get_attr(p, i) << " ";
+	cout << endl;
+    }
+
+    at->print(cout);
+
+    cout << "After print:" << endl;
+    for (p = at->first_attr(); p; at->next_attr(p)) {
+	cout << at->get_name(p) << " " << at->get_type(p) << " ";
+	for (int i = 0; i < at->get_attr_num(p); ++i)
+	     cout << at->get_attr(p, i) << " ";
+	cout << endl;
+    }
+}
+
+#ifdef NEVER
     atp->append_attr("month", "String", "Feb");
     atp->append_attr("month", "String", "Feb");
     atp->append_attr("Date", "Int32", "12345");
@@ -222,4 +330,5 @@ load_attr_table_ptr(AttrTable *atp)
 	     << atp->get_attr(p) << endl;
     }
 }
+#endif
 
