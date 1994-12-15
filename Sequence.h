9f3f400fd2acbae31b9918f81e3d6a047b5331ab
Dan Holloway <dholloway@opendap.org>
1994-12-15 21:21:54 +0000

    Modified class Sequence inheritance hierarchy, now directly inherits from class BaseType.

--- Sequence.h
+++ Sequence.h
@@ -13,11 +13,15 @@
 // jhrg 9/14/94
 
 /* $Log: Sequence.h,v $
-/* Revision 1.4  1994/11/22 14:06:04  jimg
-/* Added code for data transmission to parts of the type hierarchy. Not
-/* complete yet.
-/* Fixed erros in type hierarchy headers (typos, incorrect comments, ...).
+/* Revision 1.5  1994/12/15 21:21:54  dan
+/* Modified class Sequence inheritance hierarchy, now directly inherits
+/* from class BaseType.
 /*
+ * Revision 1.4  1994/11/22  14:06:04  jimg
+ * Added code for data transmission to parts of the type hierarchy. Not
+ * complete yet.
+ * Fixed erros in type hierarchy headers (typos, incorrect comments, ...).
+ *
  * Revision 1.3  1994/10/17  23:34:50  jimg
  * Added code to print_decl so that variable declarations are pretty
  * printed.
@@ -37,24 +41,44 @@
 #endif
 
 #include <SLList.h>
-#include "Structure.h"
+#include "BaseType.h"
 
 #include "config.h"
 #ifdef TRACE_NEW
 #include "trace_new.h"
 #endif
 
-class Sequence: public Structure {
+class Sequence: public BaseType {
 private:
+    SLList<BaseTypePtr> vars;
+
     void duplicate(const Sequence &s);
 
 public:
-    Sequence(const String &n = (char *)0, const String &t = "Sequence");
+    Sequence(const String &n = (char *)0, FILE *in = stdin, FILE *out = stdout);
     Sequence(const Sequence &rhs);
     virtual ~Sequence();
 
     const Sequence &operator=(const Sequence &rhs);
     virtual BaseType *ptr_duplicate();
+
+    virtual unsigned int size();
+    virtual bool read(String dataset, String var_name, String constraint);
+
+    virtual bool serialize(bool flush = false, unsigned int num = 0);
+    virtual unsigned int deserialize();
+
+    virtual BaseType *var(const String &name);
+    virtual void add_var(BaseType *, Part p = nil);
+
+    Pix first_var();
+    void next_var(Pix &p);
+    BaseType *var(Pix p);
+
+    virtual void print_decl(ostream &os, String space = "    ",
+			    bool print_semi = true);
+    virtual void print_val(ostream &os, String space = "");
+    virtual bool check_semantics(bool all = false);
 };
 
 #endif
