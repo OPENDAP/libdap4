70feef56e9e0a80682cace001f89bd01f90ae06b
James Gallagher <jgallagher@opendap.org>
1995-02-23 21:26:31 +0000

    Added declarations of set_mime_text and set_mime_binary.

--- cgi_util.h
+++ cgi_util.h
@@ -6,6 +6,8 @@
 char *name_path(char *path);
 char *fmakeword(FILE *f, const char stop, int *cl) ;
 void ErrMsgT(const char *Msgt);
+void set_mime_test();
+void set_mime_binary();
 
 extern "C" {
     FILE *NetConnect(char *AbsAddress, char *PostData);
