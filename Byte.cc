03a74f9b076d7e7f9b41f11f17e4eb13e53873b0
James Gallagher <jgallagher@opendap.org>
1995-07-09 21:29:28 +0000

    Added copyright notice.

--- Byte.cc
+++ Byte.cc
@@ -1,10 +1,47 @@
 
+/*
+  Copyright 1995 The University of Rhode Island and The Massachusetts
+  Institute of Technology
+
+  Portions of this software were developed by the Graduate School of
+  Oceanography (GSO) at the University of Rhode Island (URI) in collaboration
+  with The Massachusetts Institute of Technology (MIT).
+
+  Access and use of this software shall impose the following obligations and
+  understandings on the user. The user is granted the right, without any fee
+  or cost, to use, copy, modify, alter, enhance and distribute this software,
+  and any derivative works thereof, and its supporting documentation for any
+  purpose whatsoever, provided that this entire notice appears in all copies
+  of the software, derivative works and supporting documentation.  Further,
+  the user agrees to credit URI/MIT in any publications that result from the
+  use of this software or in any product that includes this software. The
+  names URI, MIT and/or GSO, however, may not be used in any advertising or
+  publicity to endorse or promote any products or commercial entity unless
+  specific written permission is obtained from URI/MIT. The user also
+  understands that URI/MIT is not obligated to provide the user with any
+  support, consulting, training or assistance of any kind with regard to the
+  use, operation and performance of this software nor to provide the user
+  with any updates, revisions, new versions or "bug fixes".
+
+  THIS SOFTWARE IS PROVIDED BY URI/MIT "AS IS" AND ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+  EVENT SHALL URI/MIT BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL
+  DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+  PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTUOUS
+  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE ACCESS, USE OR PERFORMANCE
+  OF THIS SOFTWARE.
+*/
+
 // Implementation for Byte.
 //
 // jhrg 9/7/94
 
 // $Log: Byte.cc,v $
-// Revision 1.11  1995/06/28 17:10:18  dan
+// Revision 1.12  1995/07/09 21:28:53  jimg
+// Added copyright notice.
+//
+// Revision 1.11  1995/06/28  17:10:18  dan
 // Modified constructor to explicitly cast return type to xdrproc_t, resolves
 // problem associated with <rpc/xdr.h> under OSF 3.X.
 //
