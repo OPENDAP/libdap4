f6d4bae1995de5d79f1e21980718aeb883fe07b5
James Gallagher <jgallagher@opendap.org>
1995-07-11 16:58:11 +0000

    Added to release-1-0-patches.

---
+++ das.tab.h
@@ -0,0 +1,16 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	ATTR	258
+#define	ID	259
+#define	INT	260
+#define	FLOAT	261
+#define	STR	262
+#define	BYTE	263
+#define	INT32	264
+#define	FLOAT64	265
+#define	STRING	266
+#define	URL	267
+
+
+extern YYSTYPE daslval;
