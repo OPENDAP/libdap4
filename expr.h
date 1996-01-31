19ff302058a7e884d847a4985b37ce279ef5fd3b
James Gallagher <jgallagher@opendap.org>
1996-01-31 18:55:15 +0000

    Added to repository.

---
+++ expr.h
@@ -0,0 +1,32 @@
+/*
+  Types for the expr parser.
+
+  11/4/95 jhrg
+*/
+
+/*
+  $Log: expr.h,v $
+  Revision 1.1  1996/01/31 18:55:15  jimg
+  Added to repository.
+
+ */
+
+#include <String.h>
+#include <SLList.h>
+
+#include "config_dap.h"
+
+#include "BaseType.h"
+
+typedef SLList<int> IntList;
+typedef SLList<IntList *> IntListList;
+
+typedef struct {
+    Type type;			/* Type is an enum defined in BaseType.h */
+    union {
+	int32 i;
+	float64 f;
+	String *s;
+    } v;
+} value;
+
