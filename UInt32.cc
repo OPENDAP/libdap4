340b40a477bf8ab165d3793a91c41d930bad8644
James Gallagher <jgallagher@opendap.org>
1996-08-26 19:45:27 +0000

    Added.

---
+++ UInt32.cc
@@ -0,0 +1,180 @@
+
+// (c) COPYRIGHT URI/MIT 1996
+// Please read the full copyright statement in the file COPYRIGH.  
+//
+// Authors:
+//      jhrg,jimg       James Gallagher (jgallagher@gso.uri.edu)
+
+// Implementation for Int32.
+//
+// jhrg 9/7/94
+
+// $Log: UInt32.cc,v $
+// Revision 1.1  1996/08/26 19:45:27  jimg
+// Added.
+//
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "config_dap.h"
+
+static char rcsid[] __unused__ = {"$Id: UInt32.cc,v 1.1 1996/08/26 19:45:27 jimg Exp $"};
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include "UInt32.h"
+#include "DDS.h"
+#include "util.h"
+#include "dods-limits.h"
+#include "debug.h"
+
+#ifdef TRACE_NEW
+#include "trace_new.h"
+#endif
+
+// *** Fix me - don't use XDR_INT32 since that will loose half the unsigned
+// values! jhrg 8/15/96
+
+UInt32::UInt32(const String &n) 
+    : BaseType(n, dods_int32_c, (xdrproc_t)XDR_INT32)
+{
+}
+
+unsigned int
+UInt32::width()
+{
+    return sizeof(dods_uint32);
+}
+
+// XDR_INT32 bug.
+
+bool
+UInt32::serialize(const String &dataset, DDS &dds, XDR *sink,
+		  bool ce_eval = true)
+{
+    int error;
+
+    if (!read_p() && !read(dataset, error))
+	return false;
+
+    if (ce_eval && !dds.eval_selection(dataset))
+	return true;
+
+    if (!XDR_INT32(sink, &_buf))
+	return false;
+
+    return true;
+}
+
+// XDR_INT32 bug.
+
+bool
+UInt32::deserialize(XDR *source, bool)
+{
+    unsigned int num = XDR_INT32(source, &_buf);
+
+    return (num > 0);		/* make the return value a boolean */
+}
+
+unsigned int
+UInt32::val2buf(void *val, bool)
+{
+    assert(val);
+
+    _buf = *(dods_uint32 *)val;
+
+    return width();
+}
+
+unsigned int
+UInt32::buf2val(void **val)
+{
+    assert(_buf && val);
+
+    if (!*val)
+	*val = new dods_uint32;
+
+    *(dods_uint32 *)*val =_buf;
+
+    return width();
+}
+
+// Print BUF to stdout with its declaration. Intended mostly for debugging.
+
+void 
+UInt32::print_val(ostream &os, String space, bool print_decl_p)
+{
+    if (print_decl_p) {
+	print_decl(os, space, false);
+	os << " = " << _buf << ";" << endl;
+    }
+    else 
+	os << _buf;
+}
+
+bool
+UInt32::ops(BaseType &b, int op)
+{
+    dods_uint32 a1, a2;
+
+    if (!read_p()) {
+	cerr << "This value not yet read!" << endl;
+	return false;
+    }
+    else {
+	dods_uint32 *a1p = &a1;
+	buf2val((void **)&a1p);
+    }
+
+    if (!b.read_p()) {
+	cerr << "Arg value not yet read!" << endl;
+	return false;
+    }
+    else switch (b.type()) {
+      case dods_byte_c:
+      case dods_int16_c:
+      case dods_uint16_c: {
+      case dods_int32_c:
+      case dods_uint32_c: {
+	dods_int32 *a2p = &a2;
+	b.buf2val((void **)&a2p);
+	break;
+      }
+      case dods_float64_c: {
+	double d;
+	double *dp = &d;
+	b.buf2val((void **)&dp);
+	a2 = (dods_int32)d;
+	break;
+      }
+      case dods_str_c: {
+	String s;
+	String *sp = &s;
+	b.buf2val((void **)&sp);
+
+	char *ptr;
+	const char *cp = (const char *)s;
+	long v = strtol(cp, &ptr, 0);
+
+	if (v == 0 && cp == ptr) {
+	    cerr << "`" << s << "' is not an integer value" << endl;
+	    return false;
+	}
+	if (v > DODS_INT_MAX || v < DODS_INT_MIN) {
+	    cerr << "`" << v << "' is not a integer value" << endl;
+	    return false;
+	}
+
+	a2 = v;
+	break;
+      }
+      default:
+	return false;
+	break;
+    }
+
+    return int_ops(a1, a2, op);
+}
---
+++ UInt32.h
@@ -0,0 +1,59 @@
+// -*- C++ -*-
+
+// (c) COPYRIGHT URI/MIT 1996
+// Please read the full copyright statement in the file COPYRIGH.  
+//
+// Authors:
+//      jhrg,jimg       James Gallagher (jgallagher@gso.uri.edu)
+
+// Interface for the UInt32 (unsigned int 32) type. 
+//
+// jhrg 9/7/94
+
+// $Log: UInt32.h,v $
+// Revision 1.1  1996/08/26 19:40:40  jimg
+// Added.
+//
+
+#ifndef _UInt32_h
+#define _UInt32_h 1
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include <rpc/types.h>
+#include <netinet/in.h>
+#include <rpc/xdr.h>
+
+#include "BaseType.h"
+
+class UInt32: public BaseType {
+protected:
+    dods_uint32 _buf;
+
+public:
+    UInt32(const String &n = (char *)0);
+    virtual ~Int32() {}
+
+    virtual BaseType *ptr_duplicate() = 0;
+    
+    virtual unsigned int width();
+
+    virtual bool serialize(const String &dataset, DDS &dds, XDR *sink,
+			   bool ce_eval = true);
+    virtual bool deserialize(XDR *source, bool reuse = false);
+
+    virtual bool read(const String &dataset, int &error) = 0;
+
+    virtual unsigned int val2buf(void *buf, bool reuse = false);
+    virtual unsigned int buf2val(void **val);
+
+    virtual void print_val(ostream &os, String space = "",
+			   bool print_decl_p = true);
+
+    virtual bool ops(BaseType &b, int op);
+};
+
+#endif
+
