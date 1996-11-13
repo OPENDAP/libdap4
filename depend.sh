d682d77c02ebea0514fcae3ac5ed75a786606e5f
James Gallagher <jgallagher@opendap.org>
1996-11-13 23:39:07 +0000

    *** empty log message ***

--- depend.sh
+++ depend.sh
@@ -6,22 +6,25 @@
 #    the backup coy of the Makefile.
 # -m <makefile name> causes depend to use <makefile name> instead of
 #    `Makefile'. 
+# -n Don't actually make the dependencies, just print out what would have been 
+#    done. 
 # jhrg 4/28/95
 #
 # Added -b option. Added `-E' to CFLAGS.
 # jhrg 3/5/96
 #
-# $Id: depend.sh,v 1.6 1996/03/05 17:57:58 jimg Exp $
+# $Id: depend.sh,v 1.7 1996/11/13 23:39:07 jimg Exp $
 
 usage="depend [-s][-b <ext>][-m <makefile name>] -- <compiler options> -- <files>"
 CFLAGS="-E -MM"
 tmp=/usr/tmp/depend$$
 makefile=Makefile
 bak=bak
+doit=1
 
 # read the command options
 
-x=`getopt sb:m: "$@"`		# "$@" preserves quotes in the input
+x=`getopt sb:m:n "$@"`		# "$@" preserves quotes in the input
 
 if [ $? != 0 ]			# $? is the exit status of 'getopt ...'
 then
@@ -40,6 +43,7 @@ do
 	-s) CFLAGS="-E -M"; shift 1;;
 	-m) makefile=$2; shift 2;;
 	-b) bak=$2; shift 2;;
+	-n) doit=0; shift 1;;
         -\?) echo $usage; exit 2;;
 	--) shift 1; break;;
     esac
@@ -53,6 +57,9 @@ do
     shift 1
 done
 shift 1				# shift past the second `--' to the files
+
+if test $doit = "1"
+then
 
 # build the new Makefile in a tmp directory using the existing makefile:
 # first copy everything upto the dependencies using awk, then compoute and
@@ -68,5 +75,13 @@ gcc $CFLAGS $* >> $tmp
 mv $makefile ${makefile}.${bak}	# backup the current Makefile 
 
 mv $tmp $makefile
+
+else
+
+echo "gcc $CFLAGS $* >> $tmp"
+echo "mv $makefile ${makefile}.${bak}"
+echo "mv $tmp $makefile"
+
+fi
 
 exit 0
