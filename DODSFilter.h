54c623d54394d2288661a4d956c074ca42fef01d
James Gallagher <jgallagher@opendap.org>
1997-09-22 23:04:59 +0000

    Added doc++ style comments.

--- DODSFilter.h
+++ DODSFilter.h
@@ -8,6 +8,9 @@
 //	jhrg,jimg	James Gallagher (jgallagher@gso.uri.edu)
 
 // $Log: DODSFilter.h,v $
+// Revision 1.2  1997/09/22 23:04:59  jimg
+// Added doc++ style comments.
+//
 // Revision 1.1  1997/08/28 20:39:02  jimg
 // Created
 //
@@ -21,10 +24,13 @@
 
 #include <String.h>
 
-/** This class contains common functionality for the filter program used to
+/** This class contains common functionality for the filter programs used to
   make up the DODS data servers. Each of these filters is called by a CGI
   script with information about the user's request. The filter writes
-  information to stdout to send a response back to the user's program. 
+  information to stdout to send a response back to the user's program. Note
+  that the filter programs do not *have* to be called by a CGI
+  script/program, but that is the normal mechanism by which they are invoked.
+  
   @author jhrg 8/26/97 */
 
 class DODSFilter {
@@ -51,8 +57,9 @@ public:
 
     virtual ~DODSFilter();
 
-    /** Class invariant. 
-      @return True is the class state is OK, false otherwise. */
+    /** Class invariant. Use this to test for correct options passed via
+      argc/argv. 
+      @return True if the class state is OK, false otherwise. */
     bool OK();
 
     /** Should the filter send version information to the client program?
@@ -64,8 +71,10 @@ public:
       @return A String object that contains the constraint expression. */
     String get_ce();
 
-    /** Get the dataset name. 
-      @return A String object that contains the constraint expression. */
+    /** Get the dataset name. We define `dataset name to be the filename or
+      other string that the filter program will use to access the data. In
+      many cases it is the name of a database.
+      @return A String object that contains the name of the dataset. */
     String get_dataset_name();
 
     /** Get the version information for the dataset. To read version
@@ -87,8 +96,8 @@ public:
     bool read_ancillary_dds(DDS &dds);
 
     /** Print usage information for a filter program. This is sent to stderr
-      and will up in the httpd log file. In addition, an error object is sent
-      back to the client program telling them that the server is hosed. */
+      and will show up in the httpd log file. In addition, an error object is
+      sent back to the client program telling them that the server is hosed. */
     void print_usage();
 
     /** Send version information back to the client program. */ 
