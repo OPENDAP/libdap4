60500bb924c90622ae2036e3605bb4ea98ac1d34
James Gallagher <jgallagher@opendap.org>
1998-02-11 02:37:23 +0000

    Added to DAP.

---
+++ deflate.c
@@ -0,0 +1,129 @@
+
+/*
+  Picked up from the w3c. 2/10/1998 jhrg
+
+  Modified: Added the -s (silent) option to suppress diagnostic messages.
+*/
+
+/* $Log: deflate.c,v $
+/* Revision 1.1  1998/02/11 02:37:23  jimg
+/* Added to DAP.
+/* */
+
+const char *rcsid={"$Id: deflate.c,v 1.1 1998/02/11 02:37:23 jimg Exp $"};
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "zlib.h"
+
+z_stream z;
+
+#define DEFAULT_BUFFER_SIZE	32*1024
+#define DEFAULT_COMPRESSION	Z_DEFAULT_COMPRESSION;
+
+#define MAX(a,b) ((a) >= (b) ? (a) : (b))
+#define MIN(a,b) ((a) <= (b) ? (a) : (b))
+
+char *input_buffer 	= NULL;
+char *output_buffer 	= NULL;
+int  buffer_size 	= DEFAULT_BUFFER_SIZE;
+int  compression_rate	= DEFAULT_COMPRESSION;
+
+FILE * fin = stdin;
+FILE * fout = stdout;
+
+void HelpScreen (const char *progname)
+{
+    fprintf(stderr, "\nDeflates using Zlib from stdin to stdout\n");
+    fprintf(stderr, "Usage: %s\n", progname ? progname : "deflate");
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "\t-b\tBuffer size (default 32K). This has no effect on compression result\n");
+    fprintf(stderr, "\t-c\tCompression rate [1..9] where 1 is compress faster and 9 is compress better (default 6)\n");
+    fprintf(stderr, "\t-s\tSilent. Print no diagnostics.\n");
+    fprintf(stderr, "\t-h\tHelp - this help screen\n");
+}
+
+int main (int argc, char ** argv)
+{
+    int arg;
+    int status;
+    int count;
+    int silent = FALSE;		/* default is to be verbose 2/10/1998 jhrg */
+
+    /* Scan command line for parameters */
+    for (arg=1; arg<argc; arg++) {
+	
+	if (*argv[arg] == '-') {
+     
+	    /* Buffer size */
+	    if (!strcmp(argv[arg], "-b")) {
+		buffer_size = (arg+1 < argc && *argv[arg+1] != '-') ?
+		    atoi(argv[++arg]) : DEFAULT_BUFFER_SIZE;
+		buffer_size = MAX(buffer_size, 256);
+
+	    /* Compression rate */
+	    } else if (!strcmp(argv[arg], "-c")) {
+		compression_rate = (arg+1 < argc && *argv[arg+1] != '-') ?
+		    atoi(argv[++arg]) : DEFAULT_COMPRESSION;
+		compression_rate = MAX(compression_rate, Z_BEST_SPEED);
+		compression_rate = MIN(compression_rate, Z_BEST_COMPRESSION);
+
+	    /* Print the help screen and exit */
+	    } else if (!strcmp(argv[arg], "-h")) {
+		HelpScreen(*argv);
+		exit(0);
+
+	    } else if (!strcmp(argv[arg], "-s")) {
+		silent = TRUE;
+
+	    } else {
+		HelpScreen(*argv);
+		exit(-1);
+	    }
+	} else {
+	    HelpScreen(*argv);
+	    exit(-1);
+	}
+    }
+
+    if ((input_buffer = (char *) calloc(1, buffer_size)) == NULL ||
+	(output_buffer = (char *) calloc(1, buffer_size)) == NULL) {
+	fprintf(stderr, "Not enough memory\n");
+	exit(-1);
+    }
+
+    deflateInit(&z, compression_rate);
+    z.avail_in = 0;
+    z.next_out = output_buffer;
+    z.avail_out = buffer_size;
+    for ( ; ; ) {
+        if ( z.avail_in == 0 ) {
+            z.next_in = input_buffer;
+            z.avail_in = fread( input_buffer, 1, buffer_size, fin );
+        }
+	if ( z.avail_in == 0 ) {
+	    status = deflate( &z, Z_FINISH );
+	    count = buffer_size - z.avail_out;
+	    if ( count ) fwrite( output_buffer, 1, count, fout );
+	    break;
+	}
+        status = deflate( &z, Z_NO_FLUSH );
+        count = buffer_size - z.avail_out;
+        if ( count ) fwrite( output_buffer, 1, count, fout );
+        z.next_out = output_buffer;
+        z.avail_out = buffer_size;
+    }
+    if (!silent)
+	fprintf(stderr, "Compressing data: raw data %lu, compressed %lu, factor %.2f, compression level (default = -1) %d, buffer size %d\n",
+		z.total_in, z.total_out,
+		z.total_in == 0 ? 0.0 :
+		(double)z.total_out / z.total_in,
+		compression_rate,
+		buffer_size);
+    deflateEnd(&z);
+    fflush(stdout);
+
+    if (input_buffer) free(input_buffer);
+    if (output_buffer) free(output_buffer);
+    return 0;
+}
