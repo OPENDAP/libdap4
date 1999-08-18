5c5e42ecf541d2bfdf3c0fc6d0a53b2d4353ead4
James Gallagher <jgallagher@opendap.org>
1999-08-18 16:03:06 +0000

    Merged changes from release-3-1-0

--- depend.sh
+++ depend.sh
@@ -8,23 +8,25 @@
 #    `Makefile'. 
 # -n Don't actually make the dependencies, just print out what would have been 
 #    done. 
-# jhrg 4/28/95
+# -t Truncate the `Makefile' after making a backup copy. 
+# jhrg 8/17/99
 #
 # Added -b option. Added `-E' to CFLAGS.
 # jhrg 3/5/96
 #
-# $Id: depend.sh,v 1.8 1998/07/09 21:12:45 jimg Exp $
+# $Id: depend.sh,v 1.9 1999/08/18 16:02:53 jimg Exp $
 
-usage="depend [-s][-b <ext>][-m <makefile name>] -- <compiler options> -- <files>"
+usage="depend [-s][-t][-b <ext>][-m <makefile name>] -- <compiler options> -- <files>"
 CFLAGS="-E -MM -MG"
 tmp=/usr/tmp/depend$$
 makefile=Makefile
 bak=bak
 doit=1
+truncate=0
 
 # read the command options
 
-x=`getopt sb:m:n "$@"`		# "$@" preserves quotes in the input
+x=`getopt stnb:m: "$@"`		# "$@" preserves quotes in the input
 
 if [ $? != 0 ]			# $? is the exit status of 'getopt ...'
 then
@@ -44,6 +46,7 @@ do
 	-m) makefile=$2; shift 2;;
 	-b) bak=$2; shift 2;;
 	-n) doit=0; shift 1;;
+	-t) truncate=1; shift 1;;
         -\?) echo $usage; exit 2;;
 	--) shift 1; break;;
     esac
@@ -61,26 +64,28 @@ shift 1				# shift past the second `--' to the files
 if test $doit = "1"
 then
 
-# build the new Makefile in a tmp directory using the existing makefile:
-# first copy everything upto the dependencies using awk, then compoute and
-# append the dependencies of the files (which are the remaining arguments).
+    if test $truncate = "0"
+    then
+	# build the new Makefile in a tmp directory using the existing 
+	# makefile: first copy everything upto the dependencies using awk,
+	# then compoute and append the dependencies of the files (which are
+	# the remaining arguments). 
+	awk 'BEGIN {found = 0}
+	    /DO NOT DELETE/ {found = 1; print $0}
+	    found != 1 {print $0}
+	    found == 1 {exit}' $makefile > $tmp
+    fi
+    gcc $CFLAGS $* >> $tmp
 
-awk 'BEGIN {found = 0}
-/DO NOT DELETE/ {found = 1; print $0}
-found != 1 {print $0}
-found == 1 {exit}' $makefile > $tmp
+    mv $makefile ${makefile}.${bak}	# backup the current Makefile 
 
-gcc $CFLAGS $* >> $tmp
-
-mv $makefile ${makefile}.${bak}	# backup the current Makefile 
-
-mv $tmp $makefile
+    mv $tmp $makefile
 
 else
 
-echo "gcc $CFLAGS $* >> $tmp"
-echo "mv $makefile ${makefile}.${bak}"
-echo "mv $tmp $makefile"
+    echo "gcc $CFLAGS $* >> $tmp"
+    echo "mv $makefile ${makefile}.${bak}"
+    echo "mv $tmp $makefile"
 
 fi
 
