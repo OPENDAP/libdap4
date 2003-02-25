19ebeb20bd903b168319a51e6f32c38dc8b8b532
James Gallagher <jgallagher@opendap.org>
2003-02-25 23:25:55 +0000

    Fixed for the latest rev of ais_database.xml.

--- AISResourcesTest.cc
+++ AISResourcesTest.cc
@@ -25,6 +25,8 @@
  
 // Tests for the AISResources class.
 
+#include <unistd.h>
+
 #include <cppunit/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/extensions/HelperMacros.h>
@@ -38,18 +40,19 @@ class AISResourcesTest:public TestFixture {
 private:
     AISResources *ais;
     string fnoc1, fnoc2, fnoc3;
-    string fnoc1_ais, fnoc2_ais;
+    string fnoc1_ais, fnoc2_ais, fnoc3_ais;
 
 public:
     AISResourcesTest() {} 
     ~AISResourcesTest() {} 
 
     void setUp() {
-	fnoc1 = "http://dodsdev.gso.uri.edu/dods-3.2/nph-dods/data/nc/fnoc1.nc";
-	fnoc2 = "http://dodsdev.gso.uri.edu/dods-3.2/nph-dods/data/nc/fnoc2.nc";
-	fnoc3 = "http://dodsdev.gso.uri.edu/dods-3.2/nph-dods/data/nc/fnoc3.nc";
-	fnoc1_ais = "http://dodsdev.gso.uri.edu/ais/fnoc1.nc.das";
-	fnoc2_ais = "http://dodsdev.gso.uri.edu/ais/fnoc2.nc.das";
+	fnoc1 = "http://localhost/dods-test/nph-dods/data/nc/fnoc1.nc";
+	fnoc2 = "http://localhost/dods-test/nph-dods/data/nc/fnoc2.nc";
+	fnoc3 = "http://localhost/dods-test/nph-dods/data/nc/fnoc3.nc";
+	fnoc1_ais = "http://localhost/ais/fnoc1.nc.das";
+	fnoc2_ais = "ais_testsuite/fnoc2_replace.das";
+	fnoc3_ais = "http://localhost/ais/fnoc3_fallback.das";
 
 	ais = new AISResources;
 
@@ -163,7 +166,7 @@ public:
     void read_database_test() {
 	try {
 	    AISResources *ais2 = new AISResources;
-	    ais2->read_database("ais_database.xml");
+	    ais2->read_database("ais_testsuite/ais_database.xml");
 
 	    ResourceVector trv1 = ais2->get_resource(fnoc1);
 	    CPPUNIT_ASSERT(trv1.size() == 1);
@@ -173,14 +176,12 @@ public:
 	    ResourceVector trv2 = ais2->get_resource(fnoc2);
 	    CPPUNIT_ASSERT(trv2.size() == 1);
 	    CPPUNIT_ASSERT(trv2[0].get_url() == fnoc2_ais);
-	    CPPUNIT_ASSERT(trv2[0].get_rule() == fallback);
+	    CPPUNIT_ASSERT(trv2[0].get_rule() == replace);
 
 	    ResourceVector trv3 = ais2->get_resource(fnoc3);
-	    CPPUNIT_ASSERT(trv3.size() == 2);
-	    CPPUNIT_ASSERT(trv3[0].get_url() == fnoc1_ais);
-	    CPPUNIT_ASSERT(trv3[0].get_rule() == overwrite);
-	    CPPUNIT_ASSERT(trv3[1].get_url() == fnoc2_ais);
-	    CPPUNIT_ASSERT(trv3[1].get_rule() == fallback);
+	    CPPUNIT_ASSERT(trv3.size() == 1);
+	    CPPUNIT_ASSERT(trv3[0].get_url() == fnoc3_ais);
+	    CPPUNIT_ASSERT(trv3[0].get_rule() == fallback);
 	}
 	catch (AISDatabaseReadFailed &adrf) {
 	    CPPUNIT_ASSERT(!"Document not well formed and/or valid!");
@@ -192,7 +193,7 @@ public:
 	    ais->write_database("dummy.xml");
 	    AISResources *ais2 = new AISResources;
 
-	    ais2->read_database("ais_database.xml");
+	    ais2->read_database("ais_testsuite/ais_database.xml");
 
 	    ResourceVector trv1 = ais2->get_resource(fnoc1);
 	    CPPUNIT_ASSERT(trv1.size() == 1);
@@ -202,14 +203,12 @@ public:
 	    ResourceVector trv2 = ais2->get_resource(fnoc2);
 	    CPPUNIT_ASSERT(trv2.size() == 1);
 	    CPPUNIT_ASSERT(trv2[0].get_url() == fnoc2_ais);
-	    CPPUNIT_ASSERT(trv2[0].get_rule() == fallback);
+	    CPPUNIT_ASSERT(trv2[0].get_rule() == replace);
 
 	    ResourceVector trv3 = ais2->get_resource(fnoc3);
-	    CPPUNIT_ASSERT(trv3.size() == 2);
-	    CPPUNIT_ASSERT(trv3[0].get_url() == fnoc1_ais);
-	    CPPUNIT_ASSERT(trv3[0].get_rule() == overwrite);
-	    CPPUNIT_ASSERT(trv3[1].get_url() == fnoc2_ais);
-	    CPPUNIT_ASSERT(trv3[1].get_rule() == fallback);
+	    CPPUNIT_ASSERT(trv3.size() == 1);
+	    CPPUNIT_ASSERT(trv3[0].get_url() == fnoc3_ais);
+	    CPPUNIT_ASSERT(trv3[0].get_rule() == fallback);
 	}
 	catch (AISDatabaseReadFailed &adrf) {
 	    CPPUNIT_ASSERT(!"Document not well formed and/or valid!");
@@ -239,10 +238,15 @@ main( int argc, char* argv[] )
 
     runner.run();
 
+    unlink("dummy.xml");
+
     return 0;
 }
 
 // $Log: AISResourcesTest.cc,v $
+// Revision 1.3  2003/02/25 23:25:55  jimg
+// Fixed for the latest rev of ais_database.xml.
+//
 // Revision 1.2  2003/02/21 00:14:24  jimg
 // Repaired copyright.
 //
