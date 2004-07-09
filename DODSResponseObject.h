1a1491f7914b0dc14c3792bdc3cdd78d1290423c
Patrick West <pwest@opendap.org>
2004-07-09 16:05:57 +0000

    Parent class of DAS and DDS

---
+++ DODSResponseObject.h
@@ -0,0 +1,16 @@
+#ifndef  DODSResponseObject_h_
+#define DODSResponseObject_h_ 1
+
+#include <string>
+
+using std::string ;
+
+class DODSResponseObject
+{
+public:
+    DODSResponseObject() {} ;
+    virtual ~DODSResponseObject() {} ;
+};
+
+#endif //DODSResponseObject_h_
+
