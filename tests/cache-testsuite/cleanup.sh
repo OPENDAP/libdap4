2612d1ce7b992aca70ed4b2267577a794ccc40ad
James Gallagher <jgallagher@opendap.org>
2005-07-12 21:40:33 +0000

    mistakenly removed alloca_.h from svn. Fixed tests/cache_testsuite.

--- tests/cache-testsuite/cleanup.sh
+++ tests/cache-testsuite/cleanup.sh
@@ -2,10 +2,12 @@
 #
 # remove stuff that accumulates between runs of the HTTPCacheTest unit tests.
 
+cp -r dods_cache/.svn svn-t
 rm -rf dods_cache
 mkdir dods_cache
 mkdir dods_cache/1306
 cp dot.index dods_cache/.index
+mv svn-t dods_cache/.svn
 
 rm -rf gc_cache
 rm -rf purge_cache
