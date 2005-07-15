b09350850ea82b03895f9d8112712f18a564b8d0
James Gallagher <jgallagher@opendap.org>
2005-07-15 17:49:28 +0000

    Moved the unit tests from 'tests' to 'unit-tests.' The unit tests are now only run when you cd into unit-tests and use make check. This change was made because those tests use network services which might not be present on some hosts used to build the package.

---
+++ unit-tests/expr-testsuite/test.b
@@ -0,0 +1,12 @@
+# Test Sequences
+
+dataset {
+	Sequence {
+		Int32 i;
+		Int32 j;
+		Sequence {
+			Float32 f;
+			Float32 g;
+		} child;
+	} parent;
+} testb;
---
+++ unit-tests/expr-testsuite/test.c
@@ -0,0 +1,16 @@
+# Test Sequences
+
+dataset {
+   Sequence {
+      Int32 i;
+      Int32 j;
+         Sequence {
+             Float32 f;
+             Float32 g;
+             Sequence {
+                Byte a;
+                Byte b;
+             } baby;
+         } child;
+   } parent;
+} testc;
