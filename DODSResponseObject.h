13e19a56ac9fa876d09ece2ffa3c7cdab9459582
James Gallagher <jgallagher@opendap.org>
2005-11-15 22:20:27 +0000

    Added commentary

--- DODSResponseObject.h
+++ DODSResponseObject.h
@@ -6,7 +6,16 @@
 using std::string ;
 
 /** @brief Abstract base class representing a specific set of information
- * for a DODS dataset, such as DAS, DDS, DODS, DDX.
+    for a DODS dataset (currently DAS and DDS).
+    
+    This class' sole purpose is to serve as a base class for DAS and DDS.
+    Code in the OPeNDAP/HOA Back End Server (BES) uses this to pass pointers
+    to DAS and DDS objects from one place to another. 
+    
+    \note The DDX and 'DODS' objects don't exist. The DDX response is generated
+    using a DDS object. Optionally the information from a DAS can be merged
+    into the DDS beforehand. The 'DODS' object is generated from a DDS once
+    data values are read into it.
  */
 class DODSResponseObject
 {
