fdadadf7f2611975553318b94fd2cb4f4a43d8d5
James Gallagher <jgallagher@opendap.org>
2006-01-12 20:17:43 +0000

    cgi_util.cc: In remove_mime_header() \n was used as the MIME header separator when CRLF should have been used. Since the rest of the code was fixed for 3.5.3, remove_mime_header() didn't find the separator. This broke the HTML form response.

--- cgi_util.cc
+++ cgi_util.cc
@@ -864,7 +864,7 @@ remove_mime_header(FILE *in)
     char tmp[256];
     while (!feof(in)) {
 	fgets(tmp, 255, in);
-	if (tmp[0] == '\n')
+	if (strncmp(&tmp[0], CRLF, 2) == 0)
 	    return true;
     }
 
