8dd5a2c2e66ae63d21e538bae208928c7657f8a4
Robert Morris <Robert.O.Morris@jpl.nasa.gov>
2006-03-05 20:47:26 +0000

    Redo recently clobbered changes from gl/ refresh

--- gl/regex_internal.h
+++ gl/regex_internal.h
@@ -22,7 +22,12 @@
 
 #include <assert.h>
 #include <ctype.h>
+#ifdef WIN32
+#define inline /*  VC++ doesn't swollow the <inline> syntax used here  */
+#include <stdbool_.h>
+#else
 #include <stdbool.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -459,7 +464,11 @@ static unsigned int re_string_context_at (const re_string_t *input,
 #define re_string_skip_bytes(pstr,idx) ((pstr)->cur_idx += (idx))
 #define re_string_set_index(pstr,idx) ((pstr)->cur_idx = (idx))
 
+#ifdef WIN32
+#include <alloca_.h>
+#else
 #include <alloca.h>
+#endif
 
 #ifndef _LIBC
 # if HAVE_ALLOCA
