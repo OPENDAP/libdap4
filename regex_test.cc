6d592bbde3aa1d6aa42e72ccec3d4ba25654007f
James Gallagher <jgallagher@opendap.org>
2006-06-02 17:52:35 +0000

    regex_test.cc: Added

---
+++ regex_test.cc
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+#include "Error.h"
+#include "GNU/GNURegex.h"
+
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+    Regex r1("^\\..*");
+    
+    int result = r1.match("test", 4, 0);
+    cerr << "r1.match(\"test\", 4, 0): " << result << endl;
+    
+    result = r1.match(".on", 3, 0);
+    cerr << "r1.match(\".on\", 3, 0): " << result << endl;
+}
