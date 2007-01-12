7fe98af26c676592eb112f15ea89116cc165c143
Patrick West <pwest@opendap.org>
2007-01-12 06:18:07 +0000

    comment headers updated with correct information and Copyright. M    DapObj.h M    DapIndent.h M    DapIndent.cc

--- DapObj.h
+++ DapObj.h
@@ -1,10 +1,12 @@
 // DapObj.h
 
-// This file is part of bes, A C++ back-end server implementation framework
-// for the OPeNDAP Data Access Protocol.
+// -*- mode: c++; c-basic-offset:4 -*-
 
-// Copyright (c) 2004,2005 University Corporation for Atmospheric Research
-// Author: Patrick West <pwest@ucar.edu> and Jose Garcia <jgarcia@ucar.edu>
+// This file is part of libdap, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2002,2003 OPeNDAP, Inc.
+// Author: Patrick West <pwest@ucar.org>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -20,17 +22,15 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-// You can contact University Corporation for Atmospheric Research at
-// 3080 Center Green Drive, Boulder, CO 80301
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
  
-// (c) COPYRIGHT University Corporation for Atmostpheric Research 2004-2005
-// Please read the full copyright statement in the file COPYRIGHT_UCAR.
+// (c) COPYRIGHT URI/MIT 1994-1999
+// Please read the full copyright statement in the file COPYRIGHT_URI.
 //
 // Authors:
 //      pwest       Patrick West <pwest@ucar.edu>
-//      jgarcia     Jose Garcia <jgarcia@ucar.edu>
 
-/** @brief top level BES object to house generic methods
+/** @brief top level DAP object to house generic methods
  */
 
 #ifndef A_DapObj_h
