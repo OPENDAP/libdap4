f8f6ca2a6dcb2406b7881cf69e0f685f03a4f3c2
Rikki Duncan <rduncan@opendap.org>
2009-12-23 19:55:11 +0000

    changes for win32 build

--- win32/gl/configmake.h
+++ win32/gl/configmake.h
@@ -1,12 +1,24 @@
-/*
- * configmake.h
- *
- *  Created on: Dec 23, 2009
- *      Author: rikki
- */
-
-#ifndef CONFIGMAKE_H_
-#define CONFIGMAKE_H_
-
-
-#endif /* CONFIGMAKE_H_ */
+#define PREFIX "/usr/local"
+#define EXEC_PREFIX "/usr/local"
+#define BINDIR "/usr/local/bin"
+#define SBINDIR "/usr/local/sbin"
+#define LIBEXECDIR "/usr/local/libexec"
+#define DATAROOTDIR "/usr/local/share"
+#define DATADIR "/usr/local/share"
+#define SYSCONFDIR "/usr/local/etc"
+#define SHAREDSTATEDIR "/usr/local/com"
+#define LOCALSTATEDIR "/usr/local/var"
+#define INCLUDEDIR "/usr/local/include"
+#define OLDINCLUDEDIR "/usr/include"
+#define DOCDIR "/usr/local/share/doc/libdap"
+#define INFODIR "/usr/local/share/info"
+#define HTMLDIR "/usr/local/share/doc/libdap"
+#define DVIDIR "/usr/local/share/doc/libdap"
+#define PDFDIR "/usr/local/share/doc/libdap"
+#define PSDIR "/usr/local/share/doc/libdap"
+#define LIBDIR "/usr/local/lib"
+#define LOCALEDIR "/usr/local/share/locale"
+#define MANDIR "/usr/local/share/man"
+#define PKGDATADIR "/usr/local/share/libdap"
+#define PKGINCLUDEDIR "/usr/local/include/libdap"
+#define PKGLIBDIR "/usr/local/lib/libdap"
