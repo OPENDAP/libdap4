f5c7de576666381ecfba78d6fad91c4ce3b95df1
Nathan Potter <ndp@opendap.org>
2012-12-06 03:51:58 +0000

    libdap: Incremental debugging check-in for ugrid_restrict (ugr2) code.

--- gf2.cc
+++ gf2.cc
@@ -1602,7 +1602,7 @@ void function_ugr2(int argc, BaseType * argv[], DDS &dds, BaseType **btpp) {
 
 			// FIXME Why make a new GF::GRidField from the result of the RestrictOp . Is this neccessary? Seems like a waste.
 			GF::GridField *resultGF = op.getResult();
-			tdmt->resultGridField = new GF::GridField(resultGF);
+			tdmt->resultGridField = resultGF; //new GF::GridField(resultGF);
 			// delete resultGF; //FIXME  Should we be deleting the intermediate??
 
 			// FIXME fix the names of the variables in the mesh_topology attributes
