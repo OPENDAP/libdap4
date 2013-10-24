37cd83a1fefc3ac0ca12167dc1b2c299659e7a43
James Gallagher <jgallagher@opendap.org>
2013-10-24 23:55:58 +0000

    All tests now pass for array projections for server function arguments.

--- tests/TestFunction.cc
+++ tests/TestFunction.cc
@@ -26,13 +26,10 @@
 
 #include "config.h"
 
-#include <sstream>
-
 #include <BaseType.h>
 #include <TestFloat64.h>
 #include <TestStr.h>
 #include <TestArray.h>
-// #include <Grid.h>
 
 #include <Error.h>
 #include <DDS.h>
@@ -43,100 +40,6 @@
 #include "TestFunction.h"
 
 namespace libdap {
-
-#if 0
-// These static functions could be moved to a class that provides a more
-// general interface for COARDS/CF someday. Assume each BaseType comes bundled
-// with an attribute table.
-
-static double string_to_double(const char *val)
-{
-    istringstream iss(val);
-    double v;
-    iss >> v;
-
-    double abs_val = fabs(v);
-    if (abs_val > DODS_DBL_MAX || (abs_val != 0.0 && abs_val < DODS_DBL_MIN))
-        throw Error(malformed_expr,string("Could not convert the string '") + val + "' to a double.");
-
-    return v;
-}
-
-/** Look for any one of a series of attribute values in the attribute table
- for \e var. This function treats the list of attributes as if they are ordered
- from most to least likely/important. It stops when the first of the vector of
- values is found. If the variable (var) is a Grid, this function also looks
- at the Grid's Array for the named attributes. In all cases it returns the
- first value found.
- @param var Look for attributes in this BaseType variable.
- @param attributes A vector of attributes; the first one found will be returned.
- @return The attribute value in a double. */
-static double get_attribute_double_value(BaseType *var, vector<string> &attributes)
-{
-    // This code also builds a list of the attribute values that have been
-    // passed in but not found so that an informative message can be returned.
-    AttrTable &attr = var->get_attr_table();
-    string attribute_value = "";
-    string values = "";
-    vector<string>::iterator i = attributes.begin();
-    while (attribute_value == "" && i != attributes.end()) {
-        values += *i;
-        if (!values.empty())
-            values += ", ";
-        attribute_value = attr.get_attr(*i++);
-    }
-
-    // If the value string is empty, then look at the grid's array (if it's a
-    // grid) or throw an Error.
-    if (attribute_value.empty()) {
-        if (var->type() == dods_grid_c)
-            return get_attribute_double_value(dynamic_cast<Grid&>(*var).get_array(), attributes);
-        else
-            throw Error(malformed_expr,string("No COARDS/CF '") + values.substr(0, values.length() - 2)
-                    + "' attribute was found for the variable '"
-                    + var->name() + "'.");
-    }
-
-    return string_to_double(remove_quotes(attribute_value).c_str());
-}
-
-static double get_attribute_double_value(BaseType *var, const string &attribute)
-{
-    AttrTable &attr = var->get_attr_table();
-    string attribute_value = attr.get_attr(attribute);
-
-    // If the value string is empty, then look at the grid's array (if it's a
-    // grid or throw an Error.
-    if (attribute_value.empty()) {
-        if (var->type() == dods_grid_c)
-            return get_attribute_double_value(dynamic_cast<Grid&>(*var).get_array(), attribute);
-        else
-            throw Error(malformed_expr,string("No COARDS '") + attribute
-                    + "' attribute was found for the variable '"
-                    + var->name() + "'.");
-    }
-
-    return string_to_double(remove_quotes(attribute_value).c_str());
-}
-
-static double get_y_intercept(BaseType *var)
-{
-    vector<string> attributes;
-    attributes.push_back("add_offset");
-    attributes.push_back("add_off");
-    return get_attribute_double_value(var, attributes);
-}
-
-static double get_slope(BaseType *var)
-{
-    return get_attribute_double_value(var, "scale_factor");
-}
-
-static double get_missing_value(BaseType *var)
-{
-    return get_attribute_double_value(var, "missing_value");
-}
-#endif
 
 /**
  * @brief scale a scalar or array variable
@@ -170,7 +73,7 @@ function_scale(int argc, BaseType * argv[], DDS &, BaseType **btpp)
     BaseType *dest = 0;
     double *data;
     if (argv[0]->is_vector_type()) {
-        Array &source = static_cast<Array&>(*argv[0]);
+        TestArray &source = static_cast<TestArray&>(*argv[0]);
         source.read();
 
         data = extract_double_array(&source);
@@ -178,9 +81,9 @@ function_scale(int argc, BaseType * argv[], DDS &, BaseType **btpp)
         for (int i = 0; i < length; ++i)
             data[i] = data[i] * m;
 
-        Array *result = new Array(source);
+        Array *result = new TestArray(source);
 
-        result->add_var_nocopy(new Float64(source.name()));
+        result->add_var_nocopy(new TestFloat64(source.name()));
         result->set_value(data, length);
 
         delete[] data; // set_value copies.
