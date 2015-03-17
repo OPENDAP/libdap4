9b0a93648420146ba40c533f002eb3a88431a8a1
James Gallagher <jgallagher@opendap.org>
2015-03-17 10:08:51 -0600

    Refactored TestCommon
    Now TestCommon.cc is not used; the constructors, etc. are now
    in TestCommon.h. In the future this class could be modified so
    that the 'series_values' property was accessible from TestCommon
    and not just the individual type classes.

--- tests/TestCommon.cc
+++ tests/TestCommon.cc
@@ -1,8 +1,12 @@
+
+// This file is no longer used. jhrg 3/12/15
+
 #include "TestCommon.h"
 #include "InternalErr.h"
 
 using namespace libdap ;
 
+#if 0
 TestCommon::TestCommon()
 {
 }
@@ -10,7 +14,8 @@ TestCommon::TestCommon()
 TestCommon::~TestCommon()
 {
 }
-
+#endif
+#if 0
 void
 TestCommon::set_series_values(bool)
 {
@@ -22,3 +27,4 @@ TestCommon::get_series_values()
 {
     throw InternalErr(__FILE__, __LINE__, "Unimplemented");
 }
+#endif
--- tests/TestCommon.h
+++ tests/TestCommon.h
@@ -28,40 +28,37 @@
 #ifndef _test_common_h
 #define _test_common_h 1
 
-#include <stdio.h>
+// #include <stdio.h>
 #include <iostream>
 
-/** Get the value of the series_value property. If true, the TestByte, ...,
-    classes should produce values that vary in a fashion similar to the DTS.
-    If false, they should exhibit the old behavior where the values are static.
-    For arrays, grids and structures the 'series_value' doesn't mean much, but
-    for sequences it's a big deal since those are constrained by value in
-    addition to by position. */
 class TestCommon {
 public:
-    TestCommon();
-    virtual ~TestCommon();
+    TestCommon() { }
+    virtual ~TestCommon() { }
 
-    /** Write out values first testing send_p() to ensure the variable has
-        data. This method is to be used when the intern_data() method is used
-        to read data into variables after the CE is evaluated. In most cases
-        variables will be used on either the server- or client-side but not
-        both. However, in some cases the same variables are used on both
-        sides. The intern_data() method is then used to read data into the
-        variables (usually serialize() is used to read the data int a
-        variable and send it to a client, then deserialize() is used to read
-        those values from a network. The intern_data() method combines those
-        operations.
+    /** Write out values. Does not test the read_p property first.
 
         @note This method is used to test the result of calling
-        intern_data(). The print_val() method does not know to check the
-        send_p() flag and thus is not useful for testing intern_data()
+        intern_data().
 
         @param out Where to write the values */
     virtual void output_values(std::ostream &out) = 0;
 
-    virtual void set_series_values(bool);
-    virtual bool get_series_values();
+    /**
+     * Set the series_value property.
+     * @see get_series_values
+     * @param state True if the values returned by read() should mimic the DTS,
+     * false if they should be static.
+     */
+    virtual void set_series_values(bool state) = 0;
+
+    /** Get the value of the series_value property. If true, the TestByte, ...,
+        classes should produce values that vary in a fashion similar to the DTS.
+        If false, they should exhibit the old behavior where the values are static.
+        For arrays, grids and structures the 'series_value' doesn't mean much, but
+        for sequences it's a big deal since those are constrained by value in
+        addition to by position. */
+    virtual bool get_series_values() = 0;
 };
 
 #endif // _test_common_h
