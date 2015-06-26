bdc584edaed0068d44e1a7206836c8f5ce6b1d52
James Gallagher <jgallagher@opendap.org>
2015-06-26 15:23:15 -0600

    Memory leak in TestArray.cc plugged

--- tests/TestArray.cc
+++ tests/TestArray.cc
@@ -196,19 +196,21 @@ void TestArray::m_build_special_values()
     }
     else if (name().find("lat") != string::npos) {
         int array_len = length();
-        double *lat_data = new double[array_len];
+        // double *lat_data = new double[array_len];
+        vector<double> lat_data(array_len);
         for (int i = 0; i < array_len; ++i) {
             lat_data[i] = 90 - (180 / array_len) * (i + 1);
         }
-        libdap::set_array_using_double(this, lat_data, array_len);
+        libdap::set_array_using_double(this, &lat_data[0], array_len);
     }
     else if (name().find("lon") != string::npos) {
         int array_len = length();
-        double *lon_data = new double[array_len];
+        //double *lon_data = new double[array_len];
+        vector<double> lon_data(array_len);
         for (int i = 0; i < array_len; ++i) {
             lon_data[i] = (360 / array_len) * (i + 1);
         }
-        libdap::set_array_using_double(this, lon_data, array_len);
+        libdap::set_array_using_double(this, &lon_data[0\], array_len);
     }
     else {
         throw InternalErr(__FILE__, __LINE__, "Unrecognized name");
