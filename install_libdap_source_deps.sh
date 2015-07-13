d842ac1be9e5362e3646eea430f364c3757078f9
James Gallagher <jgallagher@opendap.org>
2015-07-12 23:36:45 -0700

    travis caching
    Modified travis.yml and install_libdap4_source_deps.sh so that
    the bison source build is cached.

--- install_libdap_source_deps.sh
+++ install_libdap_source_deps.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 set -e
-wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
-tar -xzf bison-3.0.4.tar.gz
-cd bison-3.0.4 && ./configure --prefix=$HOME/bison && make -j 4 && make install
+
+# check to see if bison folder is empty
+if test ! -d "$HOME/bison/bin"
+then
+  wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
+  tar -xzf bison-3.0.4.tar.gz
+  cd bison-3.0.4 && ./configure --prefix=$HOME/bison && make -j 4 && make install
+else
+  echo 'Using cached bison directory.';
+fi
