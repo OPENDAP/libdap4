f32435e5b3b78360ef8916f1188fe8dd8ff5099e
James Gallagher <jgallagher@opendap.org>
2015-07-12 23:32:22 -0600

    Switched travis to use docker/containers

---
+++ install_libdap_source_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
+tar -xzf bison-3.0.4.tar.gz
+cd bison-3.0.4 && ./configure --prefix=$HOME/bison && make -j 4 && make install
+
