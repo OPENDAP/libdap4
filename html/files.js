6499cafbb1c8593f3c8078752639a9e8119232cb
James Gallagher <jgallagher@opendap.org>
2017-05-02 13:44:37 -0600

    Added docs

---
+++ html/files.js
@@ -0,0 +1,7 @@
+var files =
+[
+    [ "config.h", "config_8h_source.html", null ],
+    [ "dods-datatypes-config.h", "dods-datatypes-config_8h_source.html", null ],
+    [ "main_page.doxygen", "main__page_8doxygen_source.html", null ],
+    [ "xdr-datatypes-config.h", "xdr-datatypes-config_8h_source.html", null ]
+];
---
+++ html/navtreeindex0.js
@@ -0,0 +1,15 @@
+var NAVTREEINDEX0 =
+{
+"config_8h_source.html":[2,0,0],
+"dods-datatypes-config_8h_source.html":[2,0,1],
+"files.html":[2,0],
+"index.html":[],
+"index.html#classes":[1,1],
+"index.html#conventions":[1],
+"index.html#general":[1,0],
+"index.html#intro":[0],
+"index.html#methods":[1,2],
+"main__page_8doxygen_source.html":[2,0,2],
+"pages.html":[],
+"xdr-datatypes-config_8h_source.html":[2,0,3]
+};
