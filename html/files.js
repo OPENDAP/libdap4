bda7b03b81731735a6c49a6a5dfa1bf81795b196
James Gallagher <jgallagher@opendap.org>
2018-01-30 17:27:21 -0700

    Added new docs

---
+++ html/files.js
@@ -0,0 +1,9 @@
+var files =
+[
+    [ "config.h", "config_8h_source.html", null ],
+    [ "dods-datatypes-config.h", "dods-datatypes-config_8h_source.html", null ],
+    [ "dods-datatypes.h", "dods-datatypes_8h_source.html", null ],
+    [ "main_page.doxygen", "main__page_8doxygen_source.html", null ],
+    [ "xdr-datatypes-config.h", "xdr-datatypes-config_8h_source.html", null ],
+    [ "xdr-datatypes.h", "xdr-datatypes_8h_source.html", null ]
+];
---
+++ html/navtreedata.js
@@ -0,0 +1,22 @@
+var NAVTREE =
+[
+  [ "libdap", "index.html", [
+    [ "Introduction", "index.html#intro", null ],
+    [ "Doxygen Conventions", "index.html#conventions", [
+      [ "General", "index.html#general", null ],
+      [ "Classes", "index.html#classes", null ],
+      [ "Methods and Functions", "index.html#methods", null ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"config_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
