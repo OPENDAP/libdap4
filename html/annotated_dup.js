6c571eae8464e81ab35cf40cbfeafa32f263870d
James Gallagher <jgallagher@opendap.org>
2018-01-30 17:08:39 -0700

    Removed old docs

--- html/annotated_dup.js
+++
@@ -1,9 +0,0 @@
-var annotated_dup =
-[
-    [ "libdap", "namespacelibdap.html", "namespacelibdap" ],
-    [ "binary_function", "classbinary__function.html", null ],
-    [ "Crc32", "classCrc32.html", "classCrc32" ],
-    [ "d4_ceFlexLexer", "classd4__ceFlexLexer.html", null ],
-    [ "d4_functionFlexLexer", "classd4__functionFlexLexer.html", null ],
-    [ "GetOpt", "classGetOpt.html", "classGetOpt" ]
-];
--- html/classlibdap_1_1DapXmlNamspaces.js
+++
@@ -1,4 +0,0 @@
-var classlibdap_1_1DapXmlNamspaces =
-[
-    [ "~DapXmlNamspaces", "classlibdap_1_1DapXmlNamspaces.html#a536f625a02210fe23f7de681f22a7036", null ]
-];
