e30c01d28306260d05b882276e0010c3fe218a33
Ed Hartnett <37277297+captainkirk99@users.noreply.github.com>
2018-03-17 07:27:18 -0600

    attempting to fix second problem with travis

--- unit-tests/UInt16Test.cc
+++ unit-tests/UInt16Test.cc
@@ -97,18 +97,18 @@ public:
 
     CPPUNIT_TEST_SUITE(UInt16Test);
 
-    CPPUNIT_TEST(cons_UInt16_test);
-    CPPUNIT_TEST(checksum_test);
-    CPPUNIT_TEST(val2buf_test);
-    CPPUNIT_TEST(buf2val_test);
-    CPPUNIT_TEST(set_value_test);
-    CPPUNIT_TEST(equals_test);
+    // CPPUNIT_TEST(cons_UInt16_test);
+    // CPPUNIT_TEST(checksum_test);
+    // CPPUNIT_TEST(val2buf_test);
+    // CPPUNIT_TEST(buf2val_test);
+    // CPPUNIT_TEST(set_value_test);
+    // CPPUNIT_TEST(equals_test);
     // CPPUNIT_TEST(type_compare_test);
-    // CPPUNIT_TEST(ops_exception_1_test);
-    // CPPUNIT_TEST(ops_exception_2_test);
-    // CPPUNIT_TEST(dump_test);
-    // CPPUNIT_TEST(print_test);
-    // CPPUNIT_TEST(check_types);
+    CPPUNIT_TEST(ops_exception_1_test);
+    CPPUNIT_TEST(ops_exception_2_test);
+    CPPUNIT_TEST(dump_test);
+    CPPUNIT_TEST(print_test);
+    CPPUNIT_TEST(check_types);
 
     CPPUNIT_TEST_SUITE_END();
 
