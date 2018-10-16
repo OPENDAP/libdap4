c90b6756635a173ea9baacad760e9d8822ded405
James Gallagher <jgallagher@opendap.org>
2018-10-16 16:24:04 -0600

    Fixed paths in build-rpm.sh

--- build-rpm.sh
+++ build-rpm.sh
@@ -10,18 +10,17 @@ set -eux
 # This script will start with /home as the CWD since that's how the 
 # centos6/7 hyrax build containers are configured. The PATH will be 
 # set to include $prefix/bin and $prefix/deps/bin; $prefix will be
-# /home/install
+# /root/install. $HOME is /root for the build container.
 
-df -h
 printenv
 pwd
 
-# load the correct hyrax-dependencies into /home/install (i.e., $prefix)
+# load the correct hyrax-dependencies into $HOME/install (i.e., $prefix)
 # The packages ae 
 (cd /tmp && curl -s -O https://s3.amazonaws.com/opendap.travis.build/hyrax-dependencies-centos7-static.tar.gz) 
 tar -C $HOME -xzvf /tmp/hyrax-dependencies-centos7-static.tar.gz
 
-ls -lR /home
+ls -lR /root
 
-# This will leave the package in /home/rpmbuild/RPMS/x86_64/*.rpm
-(cd /home/libdap4 && ./configure --disable-dependency-tracking --prefix=$prefix && make -j4 rpm)
+# This will leave the package in $HOME/rpmbuild/RPMS/x86_64/*.rpm
+(cd $HOME/libdap4 && ./configure --disable-dependency-tracking --prefix=$prefix && make -j4 rpm)
