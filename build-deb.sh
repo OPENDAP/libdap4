31fdae2e8cc2f434413519b87c0cccf7f81ccbb3
Kodi Neumiller <kneumiller@mtech.edu>
2018-12-31 10:49:55 -0700

    Renamed the traivs dir

--- build-deb.sh
+++ build-deb.sh
@@ -12,15 +12,13 @@ echo "pwd = `pwd`"
 # Get the pre-built dependencies (all static libraries)
 (cd /tmp && curl -s -O https://s3.amazonaws.com/opendap.travis.build/hyrax-dependencies-$os-static.tar.gz)
 
-cd $HOME
+cd $travis_build_dir
 
 # This dumps the dependencies in $HOME/install/deps/{lib,bin,...}
 tar -xzvf /tmp/hyrax-dependencies-ubuntu14-static.tar.gz
 
 # Get a fresh copy of the sources
 git clone https://github.com/opendap/libdap4
-
-cd $TRAVIS_BUILD_DIR
 
 # build (autoreconf; configure, make)
 autoreconf -fiv
