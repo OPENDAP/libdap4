cfc1b0ab5c6f83d32010161ff7f170814efc31bb
James Gallagher <jgallagher@opendap.org>
2019-07-08 14:06:53 -0600

    Removed old docs

--- html/classlibdap_1_1fpistream.js
+++
@@ -1,5 +0,0 @@
-var classlibdap_1_1fpistream =
-[
-    [ "fpistream", "classlibdap_1_1fpistream.html#a87b68645511cb36236ba1dc7d552f733", null ],
-    [ "buf", "classlibdap_1_1fpistream.html#ab7d4682d3d7715028e3d770e5a13657f", null ]
-];
