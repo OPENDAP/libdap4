962b2374d0c1a54a3ba81b610394689a7e6e1759
James Gallagher <jgallagher@opendap.org>
2019-11-01 10:11:31 -0600

    Modified build-deb.sh to use the aws cli.
    This change was needed because the travis.build bucket is no longer
    public.

--- build-deb.sh
+++ build-deb.sh
@@ -9,13 +9,17 @@ printenv
 
 echo "pwd = `pwd`"
 
-# Get the pre-built dependencies (all static libraries)
-(cd /tmp && curl -s -O https://s3.amazonaws.com/opendap.travis.build/hyrax-dependencies-$os-static.tar.gz)
+# Get the pre-built dependencies (all static libraries). The env vars $os along with the
+# AWS public and secret key are put in the environment using 'docker run --env ...'
+# jhrg 11/1/19
+aws s3 cp s3://opendap.travis.build/hyrax-dependencies-$os-static.tar.gz /tmp/
+
+# (cd /tmp && curl -s -O https://s3.amazonaws.com/opendap.travis.build/hyrax-dependencies-$os-static.tar.gz)
 
 cd $HOME
 
 # This dumps the dependencies in $HOME/install/deps/{lib,bin,...}
-tar -xzvf /tmp/hyrax-dependencies-ubuntu14-static.tar.gz
+tar -xzvf /tmp/hyrax-dependencies-$os-static.tar.gz
 
 # Get a fresh copy of the sources
 git clone https://github.com/opendap/libdap4
