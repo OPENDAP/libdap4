6b45ba90accff7acf50f2ea20a919a529d32f461
ndp-opendap <ndp@opendap.org>
2020-02-08 08:40:15 -0800

    Testing RPM build

--- travis/rpm-to-package-dir.sh
+++ travis/rpm-to-package-dir.sh
@@ -11,19 +11,21 @@ DIST=${1}
 PKG_NAME=libdap
 
 # 'prefix' and 'TRAVIS_BUILD_DIR' are in the environment
-
+BUILD_RPM_FILES="${prefix}/rpmbuild/RPMS/x86_64/*"
 # Copied the RPM files with version numbers.
-cp ${prefix}/rpmbuild/RPMS/x86_64/* ${TRAVIS_BUILD_DIR}/package/
+cp ${BUILD_RPM_FILES} ${TRAVIS_BUILD_DIR}/package/
 
 # Now make a second set of copies with 'snapshot' in place of the version
 
 ver=`basename ${prefix}/rpmbuild/RPMS/x86_64/${PKG_NAME}-[-0-9.]*.rpm | sed -e "s|${PKG_NAME}-||g" -e "s|.${DIST}.x86_64.rpm||g"`
 
-for file in ${prefix}/rpmbuild/RPMS/x86_64/*
+for file in ${BUILD_RPM_FILES}
 do
     echo "Updating ${PKG_NAME} ${DIST} snapshot with ${file}"
     snap=`basename ${file} | sed "s|${ver}|snapshot|g"`
     cp ${file} ${TRAVIS_BUILD_DIR}/package/${snap}
 done
 
-exit 0
+ls -l ${TRAVIS_BUILD_DIR}/package/
+
+exit 1
