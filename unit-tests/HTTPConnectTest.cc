b2e08946b19b2eb1f3a6425b9c862578f68ff23f
James Gallagher <jgallagher@opendap.org>
2021-01-28 11:08:37 -0700

    Added a seconf 'check' target to run HTTPConnectTest in debug mode
    I'm tracking down an error that has appeared in code that has not
    changed in 16 years and happens only sporadically. The errors may
    be in Travis itself since our GH Action CI builds don't show any
    of these issues. But, it's a poor carpenter that blames tools.

--- unit-tests/HTTPConnectTest.cc
+++ unit-tests/HTTPConnectTest.cc
@@ -39,12 +39,13 @@
 #include "debug.h"
 
 #include "test_config.h"
+#include "GetOpt.h"
 
 using namespace CppUnit;
 using namespace std;
 
-#include"GetOpt.h"
-
+// FIXME By default this should be false, but we're tracking down a
+//  potential bug in this code or test. jhrg 1/28/21
 static bool debug = false;
 
 #undef DBG
@@ -486,7 +487,7 @@ public:
 
     void fetch_url_test_4_cpp()
     {
-        DBG(cerr << "Entering fetch_url_test 4" << endl);
+        DBG(cerr << "Entering fetch_url_test_4_cpp" << endl);
         http->set_use_cpp_streams(true);
 
         HTTPResponse *stuff = 0;
