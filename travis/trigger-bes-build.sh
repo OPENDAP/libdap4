1f54a6a4ce8e441937f5c8aaa8d8e8a642fbcb96
James Gallagher <jgallagher@opendap.org>
2021-03-29 14:53:51 -0600

    Fixed a typo in trigger-bes-build.sh

--- travis/trigger-bes-build.sh
+++ travis/trigger-bes-build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-# Given that the BES has just pushed a new set of packages, built with the libdap
-# RPMs, grab those and use them to make a new set of Docker containers. The
-# hyrax-docker git repo runs its own build to do this (and can be triggered
-# separately).
+# Once a new set of packages for libdap are pushed to S3, trigger a BES
+# build.
 
 set -e
 
-echo "-- -- -- -- -- -- -- -- -- after_deploy BEGIN -- -- -- -- -- -- -- -- --"
-
 echo "New CentOS-7 snapshot of libdap4 has been pushed. Triggering a BES build..."
+
+LIBDAP4_SNAPSHOT="libdap4-`cat VERSION` `date \"+%FT%T%z\"`"
+echo "libdap4-snapshot record: ${LIBDAP_SNAPSHOT}"  >&2
 
 git clone --depth 1 https://github.com/opendap/bes
 git config --global user.name "The-Robot-Travis"
@@ -18,14 +17,9 @@ git config --global user.email "npotter@opendap.org"
 cd bes
 git checkout master
 
-LIBDAP4_SNAPSHOT="libdap4-""rsion.build> "`date "+%FT%T%z"`
-
-echo "libdap4-snapshot record: ${LIBDAP_SNAPSHOT}"  >&2
 echo "${LIBDAP4_SNAPSHOT}" > libdap4-snapshot
 
 cat libdap4-snapshot  >&2
 
 git commit -am "${LIBDAP4_SNAPSHOT} Triggering BES build for snapshots.";
 git push https://$GIT_UID:$GIT_PSWD@github.com/opendap/bes --all;
-
-echo "-- -- -- -- -- -- -- -- -- after_deploy END -- -- -- -- -- -- -- -- --"
