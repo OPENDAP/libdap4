3429058fbef897638e02a0e7d5f6ba5f875e8e89
ndp-opendap <ndp@opendap.org>
2021-10-29 11:53:04 -0700

    More with sonar scan coverage issues

--- travis/sonar-scanner-coverage.sh
+++ travis/sonar-scanner-coverage.sh
@@ -3,11 +3,12 @@ reports_dir="gcov-reports"
 
 make coverage
 find . -wholename \*/.libs/\*.gcov -a ! -name \*\#\* -a ! -name \*.hh.gcov > gcov-files.txt
-mkdir -p ""${reports_dir}""
 echo "#----------------------------------------------------------------------"
 echo "# gcov files detected:"
 cat gcov-files.txt
 echo "#----------------------------------------------------------------------"
+
+mkdir -p "${reports_dir}"
 # gcov-reports must be configured in sonar-project.properties. jhrg 11/24/20
 mkdir -p "${gcov_dir}"
 for gcov_file in ${gcov_files}
@@ -21,11 +22,11 @@ do
         target_dir="${reports_dir}/${subdir_name}"
         mkdir -p "${target_dir}"
     fi
-    echo "Adding gcov_file: ${gcov_file} to target_dir: ${target_dir}"
+    echo "# Adding gcov_file: ${gcov_file} to target_dir: ${target_dir}"
     mv "${gcov_file}" "${target_dir}"
 done
 echo "#----------------------------------------------------------------------"
-echo "# gcov reports: "
+echo "# ${reports_dir}: "
 ls -lRa "${gcov_dir}"
 echo "#----------------------------------------------------------------------"
 
