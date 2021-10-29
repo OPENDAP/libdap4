50b10c3c53e7c5ae55a5e1ef47a0793c130bfd8c
ndp-opendap <ndp@opendap.org>
2021-10-29 10:30:43 -0700

    Working on getting the coverage files into the sonar system

--- travis/sonar-scanner-coverage.sh
+++ travis/sonar-scanner-coverage.sh
@@ -6,6 +6,22 @@ mkdir -p gcov-reports
 echo "------------------------------------------"
 cat gcov-files.txt
 echo "------------------------------------------"
-ls -l gcov-reports
 # gcov-reports must be configured in sonar-project.properties. jhrg 11/24/20
-mv -f $(cat gcov-files.txt) gcov-reports/
+gcov_dir="gcov-reports"
+mkdir -p "${gcov_dir}"
+for gcov_file in ${gcov_files}
+do
+    reports_dir="gcov-reports"
+    bname=`basename "${gcov_file}"`
+    subdir_name=`dirname "${gcov_file}" | sed -e "s|\.libs||g" -e "s|\./||g"`
+    if test -n "${subdir_name}"
+    then
+        # echo "Found SubDirectory: ${subdir_name}";
+        reports_dir="${reports_dir}/${subdir_name}"
+        mkdir -p "${reports_dir}"
+    fi
+    echo "gcov_file: ${gcov_file}"
+    echo "reports_dir: ${reports_dir}"
+    mv "${gcov_file}" "${reports_dir}"
+done
+
