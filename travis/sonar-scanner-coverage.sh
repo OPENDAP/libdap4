a54781316fedd79b9d13b3b37a531f0fecbf8405
ndp-opendap <ndp@opendap.org>
2021-10-29 11:46:29 -0700

    More with sonar scan coverage issues

--- travis/sonar-scanner-coverage.sh
+++ travis/sonar-scanner-coverage.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
+reports_dir="gcov-reports"
 
 make coverage
 find . -wholename \*/.libs/\*.gcov -a ! -name \*\#\* -a ! -name \*.hh.gcov > gcov-files.txt
-mkdir -p gcov-reports
-echo "------------------------------------------"
+mkdir -p ""${reports_dir}""
+echo "#----------------------------------------------------------------------"
+echo "# gcov files detected:"
 cat gcov-files.txt
-echo "------------------------------------------"
+echo "#----------------------------------------------------------------------"
 # gcov-reports must be configured in sonar-project.properties. jhrg 11/24/20
-gcov_dir="gcov-reports"
 mkdir -p "${gcov_dir}"
 for gcov_file in ${gcov_files}
 do
-    reports_dir="gcov-reports"
+    target_dir="${reports_dir}"
     bname=`basename "${gcov_file}"`
     subdir_name=`dirname "${gcov_file}" | sed -e "s|\.libs||g" -e "s|\./||g"`
     if test -n "${subdir_name}"
     then
         # echo "Found SubDirectory: ${subdir_name}";
-        reports_dir="${reports_dir}/${subdir_name}"
-        mkdir -p "${reports_dir}"
+        target_dir="${reports_dir}/${subdir_name}"
+        mkdir -p "${target_dir}"
     fi
-    echo "gcov_file: ${gcov_file}"
-    echo "reports_dir: ${reports_dir}"
-    mv "${gcov_file}" "${reports_dir}"
+    echo "Adding gcov_file: ${gcov_file} to target_dir: ${target_dir}"
+    mv "${gcov_file}" "${target_dir}"
 done
-echo "----------------------------------------------------------------------"
-echo "gcov files: "
+echo "#----------------------------------------------------------------------"
+echo "# gcov reports: "
 ls -lRa "${gcov_dir}"
-echo "------------------------------------------"
+echo "#----------------------------------------------------------------------"
 
