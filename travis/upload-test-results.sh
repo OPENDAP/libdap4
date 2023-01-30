13b9bdf648d41909a34875e8847b1a41b2605df4
James Gallagher <jgallagher@opendap.org>
2023-01-30 12:15:07 -0700

    test log upload script goof

--- travis/upload-test-results.sh
+++ travis/upload-test-results.sh
@@ -3,10 +3,10 @@
 # Upload the results of tests after running a build on Travis
 
 LOG_FILE_TGZ=libdap4-autotest-${TRAVIS_JOB_NUMBER}-logs.tar.gz
-if test xLIBDAP_BUILD = xmain -o xLIBDAP_BUILD = xasan -o xLIBDAP_BUILD = xdistcheck
+if test x$LIBDAP_BUILD = xmain -o x$LIBDAP_BUILD = xasan -o x$LIBDAP_BUILD = xdistcheck
 then
 	# shellcheck disable=SC2046
-	tar -czf /tmp/${LOG_FILE_TGZ} `find . -name '*.log' -print`
+	tar -czvf /tmp/${LOG_FILE_TGZ} `find . -name '*.log' -print`
 	
 	# using: 'test -z "$AWS_ACCESS_KEY_ID" || ...' keeps after_script from running
 	# the aws cli for forked PRs (where secure env vars are null). jhrg 1/30/23
