bcd87e7fdcb7d476fc0cfcb181043a3b6fe7dc67
ndp-opendap <ndp@opendap.org>
2025-01-13 13:15:57 -0800

    Trigger CI

--- travis/travis_libdap_build_offset.sh
+++ travis/travis_libdap_build_offset.sh
@@ -3,6 +3,7 @@
 # When the release numbers are edited in configure.ac, update this
 # to the current Travis number so that the 'build number' in
 # x.y.z-<build number> is zero. jhrg 3/22/21
+#
 
 export LIBDAP_TRAVIS_BUILD_OFFSET=1390
 
