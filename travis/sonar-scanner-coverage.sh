ee1742e13ca785016f921db21853b93b4f8e5024
Cloud User <rocky@ip-172-31-57-143.us-west-2.compute.internal>
2025-05-05 19:50:05 +0000

    Replace gcovr with gcov for code coverage

--- travis/sonar-scanner-coverage.sh
+++ travis/sonar-scanner-coverage.sh
@@ -4,27 +4,11 @@
 export reports_dir="gcov-reports"
 
 make coverage
-find . -wholename \*/.libs/\*.gcov -a ! -name \*\#\* -a ! -name \*.hh.gcov > gcov-files.txt
-echo "#----------------------------------------------------------------------"
-echo "# gcov files detected:"
-cat gcov-files.txt
-echo "#----------------------------------------------------------------------"
 
 mkdir -p "${reports_dir}"
-for gcov_file in `cat gcov-files.txt`
-do
-    target_dir="${reports_dir}"
-    bname=`basename "${gcov_file}"`
-    subdir_name=`dirname "${gcov_file}" | sed -e "s|\.libs||g" -e "s|\./||g"`
-    if test -n "${subdir_name}"
-    then
-        # echo "Found SubDirectory: ${subdir_name}";
-        target_dir="${reports_dir}/${subdir_name}"
-        mkdir -p "${target_dir}"
-    fi
-    echo "# Adding gcov_file: ${gcov_file} to target_dir: ${target_dir}"
-    mv "${gcov_file}" "${target_dir}"
-done
+find -name "*.gcda" | xargs gcov -b -c -r -m # Run gcov (not gcovr) on all the *.gcda files
+cp $(ls -1 *.gcov) "${reports_dir}"
+
 echo "#----------------------------------------------------------------------"
 ls -lRa "${reports_dir}"
 echo "#----------------------------------------------------------------------"
