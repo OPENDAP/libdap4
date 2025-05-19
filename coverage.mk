38db7d9e03b558a586cd14fe7d02979127898d55
James Gallagher <jgallagher@opendap.org>
2025-05-19 11:07:35 -0600

    Fixed coverage Makefile fail

--- coverage.mk
+++ coverage.mk
@@ -2,8 +2,6 @@
 # Build the code for test coverage analysis
 # jhrg 11/17/20
 
-.PHONY: coverage
-
 if ENABLE_COVERAGE
 AM_CXXFLAGS += --coverage -g -O0
 AM_LDFLAGS += --coverage -g -O0
@@ -11,7 +9,7 @@ AM_LDFLAGS += --coverage -g -O0
 # This target is for a local build that writes HTML pages.
 # The travis build scan job writes am XML file that sonarqube
 # can read. jhrg 5/18/25
-
+# .PHONY: coverage
 coverage:
 	gcovr --config gcovr.config --html --html-details --output gcovr-output/
 
