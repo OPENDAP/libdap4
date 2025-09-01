52ef4d23dfa2f8d969095c8fbbf4b6823eaed781
pvn <pedro.vicente@hushmail.com>
2025-09-01 10:30:43 -0400

    Linux: add detection of ZLIB library to fix link error

--- build.libdap.cmake.sh
+++ build.libdap.cmake.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "-- Checking dependency installations..."
+# exit immediately if a command exits with a non-zero status
+set -e  
 
 echo "-- Checking zlib installation..."
 find ../install/zlib-1.3.1/ -name "*.lib" -o -name "*.a" -o -name "zlib.h"
@@ -26,14 +27,27 @@ CMAKE_PREFIX_PATH="../install/cppunit-1.15.1;../install/libxml2-2.14.5;../instal
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
     echo "-- Detected Windows system, using .lib libraries"
     LIBXML2_LIB_PATH="../../install/libxml2-2.14.5/lib/libxml2sd.lib"
+    ZLIB_ROOT_PATH="../../install/zlib-1.3.1"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "-- Detected macOS system, using .a libraries"
     LIBXML2_LIB_PATH="../../install/libxml2-2.14.5/lib/libxml2.a"
+    ZLIB_ROOT_PATH="../../install/zlib-1.3.1"
 else
     echo "-- Detected Linux/Unix system, using .a libraries"
     LIBXML2_LIB_PATH="../../install/libxml2-2.14.5/lib/libxml2.a"
+    ZLIB_ROOT_PATH="../../install/zlib-1.3.1"
     LIBXML2_ABSOLUTE_PATH=$(realpath ../../install/libxml2-2.14.5)
 fi
+
+# find zlib library file
+ZLIB_LIB_FILE=""
+for lib_name in libz.a libzlib.a zlib.lib zlibstatic.lib; do
+    if [ -f "$ZLIB_ROOT_PATH/lib/$lib_name" ]; then
+        ZLIB_LIB_FILE="$ZLIB_ROOT_PATH/lib/$lib_name"
+        echo "-- Found zlib library: $ZLIB_LIB_FILE"
+        break
+    fi
+done
 
 cmake ../../libdap4 \
   -DCMAKE_BUILD_TYPE=Debug \
@@ -42,20 +56,16 @@ cmake ../../libdap4 \
   -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" \
   -DLibXml2_ROOT="../install/libxml2-2.14.5" \
   -DLIBXML2_LIBRARY="$LIBXML2_LIB_PATH" \
-  -DUSE_ASAN=ON \
+  -DZLIB_ROOT="$ZLIB_ROOT_PATH" \
+  -DUSE_ASAN=OFF \
   -DBUILD_DEVELOPER=ON \
   -DCMAKE_VERBOSE_MAKEFILE=ON \
   --fresh 
   
-echo "-- Press any key to build..."
+echo "-- Press any key to build libdap4..."
 read -n 1 -s
 
-cmake --build . --target dap --config Debug --verbose
-cmake --build . --target dapserver --config Debug --verbose
-cmake --build . --target dapclient --config Debug --verbose
-cmake --build . --target getdap --config Debug --verbose
-cmake --build . --target getdap4 --config Debug --verbose
-cmake --build . --target test-types --config Debug --verbose
+cmake --build . --config Debug --verbose
 cmake --install . --config Debug
 
 popd 
