ab99a51fa58d39f43bf6fab3174c30e4e312a654
James Gallagher <jgallagher@opendap.org>
2025-12-30 14:42:39 -0700

    Added baseline generation for the getdap tests

--- tests/cmake/getdap-tests.cmake
+++ tests/cmake/getdap-tests.cmake
@@ -1,13 +1,13 @@
 
 function(getdap_test test_num option url baseline xfail)
 	set(testname "getdap_test_${test_num}")
-	set(baseline "${CMAKE_CURRENT_SOURCE_DIR}/getdap-testsuite/${baseline}")
+	set(baseline_path "${CMAKE_CURRENT_SOURCE_DIR}/getdap-testsuite/${baseline}")
 	set(output "${CMAKE_CURRENT_BINARY_DIR}/${testname}.out")
 
 	set(the_test   "$<TARGET_FILE:getdap> ${option} ${url} > ${output} 2>&1")
 
 	add_test(NAME ${testname}
-			COMMAND /bin/sh -c "${the_test}; diff -b -B ${baseline} ${output} && rm -f ${output}"
+			COMMAND /bin/sh -c "${the_test}; diff -b -B ${baseline_path} ${output} && rm -f ${output}"
 	)
 
 	set_tests_properties(${testname} PROPERTIES LABELS "integration;getdap")
@@ -16,6 +16,16 @@ function(getdap_test test_num option url baseline xfail)
 	if("${xfail}" STREQUAL "xfail")
 		set_tests_properties(${testname} PROPERTIES WILL_FAIL TRUE)
 	endif()
+
+	set(staged_baseline "${CMAKE_CURRENT_BINARY_DIR}/baselines/${baseline}.base")
+	set(baseline_tgt "baseline-${testname}")
+	add_custom_target(${baseline_tgt}
+			COMMAND ${CMAKE_COMMAND} -E make_directory "$<SHELL_PATH:${CMAKE_CURRENT_BINARY_DIR}/baselines>"
+			COMMAND /bin/sh -c  "${the_test}; ${CMAKE_COMMAND} -E copy ${output} ${staged_baseline} && rm -f ${output}"
+			BYPRODUCTS "${staged_baseline}"
+			COMMENT "Staging baseline for ${testname} → ${staged_baseline}"
+			VERBATIM
+	)
 
 endfunction()
 
